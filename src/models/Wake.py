42313e39eb181c15826026de46477fa07826dc2e
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-10-17 08:44:07 -0600

    add a wake velocity placeholder method

--- src/models/Wake.py
+++ src/models/Wake.py
@@ -27,3 +27,16 @@ class Wake(BaseObject):
     def getVelocity(self):
         return self.velocity
 
+    def V(self, u, x, y, z):
+        # yDisp, zDisp = self.displ(x)
+        # wSlice = self.wakeSlicer(U, x, Y-yDisp, Z-zDisp)
+        # 
+        # # Broadcast x to make a matrix: (x + 0*y) = np.broadcast_to(x, y.shape)
+        # tempCoords = np.einsum('ij, jmn -> imn',
+        #                        self.Ri, np.stack([x + 0*Y, Y, Z]))
+        # rotorBoundary = self.tanHFilter(tempCoords[0], 0, 1)
+        # wakeBoundary = self.tanHFilter(wSlice.BT, wSlice.BV, 1)
+        # mask = rotorBoundary*wakeBoundary
+        # 
+        # return wSlice.V*mask + U*(1-mask)
+        return 1.0
