f5a83a348b53d894344bce9658b12b9b6364dd2e
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-10-16 21:49:07 -0600

    make parameters optional

--- src/models/Turbine.py
+++ src/models/Turbine.py
@@ -5,13 +5,14 @@ class Turbine(BaseObject):
 
     def __init__(self):
         super(Turbine, self).__init__()
-        self.rotorDiameter = 126.0
-        self.hubHeight = 90
-        self.numBlades = 3
-        self.pP = 1.88
-        self.pT = 2.07
-        self.generatorEfficiency = 1.0
-        self.eta = 0.768
+
+        self.rotorDiameter = None
+        self.hubHeight = None
+        self.numBlades = None
+        self.pP = None
+        self.pT = None
+        self.generatorEfficiency = None
+        self.eta = None
 
         # self.usePitch = usePitch
         # if usePitch:
