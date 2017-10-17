fbd36f147428e15ede085c459901bec063aff73a
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-10-17 08:45:19 -0600

    add a 2x2 wind farm layout

---
+++ farms/TwoByTwo.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src', 'models')))
+import src.models.Farm as Farm
+
+class TwoByTwo(Farm.Farm):
+    """
+        Describe this farm here
+    """
+    
+    def __init__(self, turbine):
+        self.turbineMap = {
+            (0,     0): turbine,
+            (800,   0): turbine,
+            (400, 800): turbine,
+            (1200,800): turbine
+        }
+        self.windSpeed = 7.0      # wind speed [m/s]
+        self.windDirection = 90.0 # wind direction [deg] (compass degrees)
