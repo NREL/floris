15efbfd1e3276c40e8c3d00e10509cb9bd8f3c42
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-10-19 08:56:21 -0600

    add more attributes and validity checks

--- src/models/FlowField.py
+++ src/models/FlowField.py
@@ -6,15 +6,27 @@ class FlowField(BaseObject):
         Describe FF here
     """
 
-    def __init__(self, wakeCombination=None):
+    def __init__(self, wakeCombination=None,
+                       windSpeed=None,
+                       shear=None,
+                       turbineCoords=None,
+                       characteristicHeight=None):
         super().__init__()
         self.wakeCombination = wakeCombination
-
+        self.windSpeed = windSpeed
+        self.shear = shear
+        self.turbineCoords = turbineCoords             # {(x,y,z), (x,y,z), ...}
+        self.characteristicHeight = characteristicHeight
+        if self.valid():
+            self.UfieldOrig = self.initializeVelocities() # initial velocities at each turbine
+        
     def valid(self):
         """
             Do validity check
         """
         valid = True
         if not super().valid():
+            return False
+        if self.characteristicHeight <= 0:
             valid = False
         return valid
