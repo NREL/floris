13bd5fd6f6970034f9187960fbcfdcd189275bdb
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-10-19 05:02:34 -0600

    move the validity check to the BaseObject

--- src/models/BaseObject.py
+++ src/models/BaseObject.py
@@ -1,4 +1,17 @@
 
 class BaseObject(object):
+    """
+        The BaseObject class is the basis for all other classes.
+
+        It is mainly a placeholder for future features. Currently implemented
+        in the base class is
+
+        valid(self) -> bool
+            checks object validity based on null attributes
+    """
+    
     def __init__(self):
         self.placeholder = "this is a placeholder for future use"
+
+    def valid(self) -> bool:
+        return None not in self.__dict__.values()
