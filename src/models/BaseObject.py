4249011725f99fe50b5ce8df05ae845cccbb3a02
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-10-30 23:08:54 -0600

    add a print statement in the basemodel validity check

--- src/models/BaseObject.py
+++ src/models/BaseObject.py
@@ -9,9 +9,19 @@ class BaseObject(object):
         valid(self) -> bool
             checks object validity based on null attributes
     """
-    
+
     def __init__(self):
         self.placeholder = "this is a placeholder for future use"
 
     def valid(self):
-        return None not in self.__dict__.values()
+        valid = True
+
+        selfvalues = self.__dict__.values()
+        if None in selfvalues:
+            valid = False
+            print("in ", self)
+            for key in self.__dict__.keys():
+                if self.__dict__[key] == None:
+                    print(key + " has value None")
+
+        return valid
