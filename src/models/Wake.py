0b04d003877dd0e759f068adb20b58e6ec589484
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-10-30 23:17:20 -0600

    remove a deprecated function, rename another one

--- src/models/Wake.py
+++ src/models/Wake.py
@@ -26,19 +26,5 @@ class Wake(BaseObject):
     def getDeflectionFunction(self):
         return self.deflection
 
-    def getVelocity(self):
+    def getVelocityFunction(self):
         return self.velocity
-
-    def V(self, u, x, y, z):
-        # yDisp, zDisp = self.displ(x)
-        # wSlice = self.wakeSlicer(U, x, Y-yDisp, Z-zDisp)
-        # 
-        # # Broadcast x to make a matrix: (x + 0*y) = np.broadcast_to(x, y.shape)
-        # tempCoords = np.einsum('ij, jmn -> imn',
-        #                        self.Ri, np.stack([x + 0*Y, Y, Z]))
-        # rotorBoundary = self.tanHFilter(tempCoords[0], 0, 1)
-        # wakeBoundary = self.tanHFilter(wSlice.BT, wSlice.BV, 1)
-        # mask = rotorBoundary*wakeBoundary
-        # 
-        # return wSlice.V*mask + U*(1-mask)
-        return 1.0
