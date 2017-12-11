256e8572f37cd806e24b546b4d3311ff1e047764
Rafael M Mudafort <rafmudaf@gmail.com>
2017-12-10 18:24:50 -0700

    configure pip install

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='floris',
+    packages=['floris'],
+    version='0.1',
+    description='Test - A wind turbine wake modeling software',
+    author='NREL NWTC',
+    author_email='rafael.mudafort@nrel.gov',
+    url='https://github.com/wisdem/floris',
+    download_url='https://github.com/wisdem/floris/archive/0.1.tar.gz',
+    keywords=['wind turbine', 'wake', 'wake modeling', 'floris', 'nrel', 'nwtc'],
+    classifiers=[],
+)
