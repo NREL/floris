0543b6110ca44aa79cb13e0ed9ce900a854e7cac
Rafael M Mudafort <rafmudaf@gmail.com>
2017-12-27 13:39:36 -0700

    improve unit test classes and methods
    - general code cleanup
    - add class definitions for buildup
    - add static test methods
    - expand test coverage of FlowField

--- tests/Coordinate_test.py
+++ tests/Coordinate_test.py
@@ -1,28 +1,41 @@
 """
 Copyright 2017 NREL
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
 
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
 """
 
-from floris.Coordinate import Coordinate
 import numpy as np
 import pytest
+from floris.Coordinate import Coordinate
 
+class CoordinateTest():
+    def __init__(self):
+        self.x, self.y = self.build_input()
+
+    def build_input(self):
+        return 1, 1
+
+# tests
 
 def test_instantiation_with_xy():
     """
     object should be instatiated with x and y
     """
-    assert Coordinate(1, 1) != None
+    test_class = CoordinateTest()
+    assert Coordinate(test_class.x, test_class.y) != None
 
 def test_rotation():
     """
-    Coordinate at 1,0 rotated 90 degrees should result in 0,1
+    Coordinate at 1,1 rotated 90 degrees should result in 1,-1
     """
-    coordinate = Coordinate(1, 0)
+    test_class = CoordinateTest()
+    coordinate = Coordinate(test_class.x, test_class.y)
     coordinate.rotate_z(np.pi/2.0)
-    assert pytest.approx(coordinate.xprime) == 0.0 and pytest.approx(coordinate.yprime) == 1.0
+    assert pytest.approx(coordinate.xprime) == -1.0 and pytest.approx(coordinate.yprime) == 1.0
--- tests/Wake_test.py
+++ tests/Wake_test.py
@@ -1,66 +1,29 @@
 """
 Copyright 2017 NREL
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
 
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
 """
 
 from floris.Wake import Wake
-from floris.WakeDeflection import WakeDeflection
-import numpy as np
-import pytest
+from .SampleInputs import SampleInputs
 
+class WakeTest():
+    def __init__(self):
+        self.sample_inputs = SampleInputs()
+        self.input_dict = self.build_input_dict()
 
-def test_instatiation():
-    """
-    # well this really needs to be made better
-    """
-    dictionary = {
-        "description": "test",
-        "properties": {
-            "velocity_model": "floris",
-            "deflection_model": "gauss",
-            "parameters": {
-                "jensen": {
-                    "we": 0.05
-                },
-                "floris": {
-                    "me": [
-                        -0.05,
-                        0.3,
-                        1.0
-                    ],
-                    "aU": 12.0,
-                    "bU": 1.3,
-                    "mU": [
-                        0.5,
-                        1.0,
-                        5.5
-                    ]
-                },
-                "gauss": {
-                    "ka": 0.3,
-                    "kb": 0.004,
-                    "alpha": 0.58,
-                    "beta": 0.077
-                },
-                "jimenez": {
-                    "kd": 0.17,
-                    "ad": 0.0,
-                    "bd": 0.0
-                },
-                "gauss_deflection": {
-                    "ka": 0.3,
-                    "kb": 0.004,
-                    "alpha": 0.58,
-                    "beta": 0.077,
-                    "ad": 0.0,
-                    "bd": 0.0
-                }
-            }
-        }
-    }
-    assert Wake(dictionary) != None
+    def build_input_dict(self):
+        return self.sample_inputs.wake
+
+# tests
+
+def test_instantiation():
+    test_class = WakeTest()
+    assert Wake(test_class.input_dict) != None
