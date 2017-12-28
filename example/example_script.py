09d6f5fc40aa9605845e07948d80662e2aa8809e
Rafael M Mudafort <rafmudaf@gmail.com>
2017-12-28 09:56:20 -0700

    shorten the lines in the license docstrings

--- example/example_script.py
+++ example/example_script.py
@@ -1,8 +1,19 @@
-import sys
-sys.path.append('../floris')
-from floris import floris
+"""
+Copyright 2017 NREL
 
-floris = floris()
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+from Floris import Floris
+
+floris = Floris()
 floris.process_input("floris.json")
 
 for coord, turbine in floris.farm.turbine_map.items():
