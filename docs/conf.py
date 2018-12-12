071aa32e847dc997513a70debd5ff076596a22f4
Rafael M Mudafort <rafmudaf@gmail.com>
2018-12-11 21:46:02 -0700

    Bug fix in doxygen path

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = [os.path.join('source', '_static')]
-html_extra_path = [os.path.join(builddir, '..', 'doxygen', 'html')]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
