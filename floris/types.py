c2f8f8a40f05a903e2c370a20cd550e272ba06e3
Paul <paul.fleming@nrel.gov>
2019-04-04 13:11:09 -0600

    Add more utility functions to types

--- floris/types.py
+++ floris/types.py
@@ -147,3 +147,19 @@ def tand(angle):
     tangent of an angle with the angle given in degrees
     """
     return np.tan(np.radians(angle))
+
+def wrap_180(x):
+    """
+    Wrap an angle to between -180 and 180
+    """  
+    x = np.where(x<=-180.,x+360.,x)
+    x = np.where(x>180.,x-360.,x)
+    return(x)
+
+def wrap_360(x):
+    """
+    Wrap an angle to between 0 and 360
+    """  
+    x = np.where(x<0.,x+360.,x)
+    x = np.where(x>=360.,x-360.,x)
+    return(x)
