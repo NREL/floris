7b843bcc4afb2d03208c004337d7ffc1627b2a80
Chris Bay <christopher.j.bay@gmail.com>
2019-04-04 17:47:24 -0600

    testing out autosummary

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,11 @@ extensions = [
     'sphinx.ext.githubpages',
     'sphinx.ext.autodoc',
     'readthedocs_ext.readthedocs',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary'
 ]
+
+autosummary_generate = True
 
 # Napoleon settings
 napoleon_google_docstring = True
@@ -75,7 +78,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_templates']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
