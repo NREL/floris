1ee1a2005b8457429539369210200f4d206c181e
Paul <paul.fleming@nrel.gov>
2019-04-15 16:31:26 -0600

    Add the data plot function

--- floris/tools/energy_ratio.py
+++ floris/tools/energy_ratio.py
@@ -2,6 +2,7 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class PlotDefaults():
     def __init__(self):
@@ -336,3 +337,96 @@ def plot_energy_ratio(reference_power_baseline,
     #     ax.set_ylim(y_lim)
 
     # return ratio_array, lower_array, upper_array, counts_array
+
+def data_plot(x,y,color='b',label='_nolegend_',x_bins=None,x_radius=None,ax=None,show_scatter=True,show_bin_points=True,show_confidence=True,min_vals=1,seaborn=False,show_80=False):
+
+    if (not ax) and (not seaborn):
+        fig, ax = plt.subplots()
+
+    if seaborn:
+        show_bin_points=False
+        show_scatter=False
+
+
+    df = pd.DataFrame({'x':x,'y':y })
+
+    if df.shape[0]>0:
+
+
+        # If bins not provided, just use ints
+        if x_bins is None:
+            x_bins = np.arange(df['x'].astype(int).min(),df['x'].astype(int).max(),1)
+
+        # if no radius provided, use bins to determine
+        if x_radius is None:
+            x_radius = (x_bins[1] - x_bins[0])/2.0
+        
+        # now loop over bins and determine stats
+        median_vals = np.zeros_like(x_bins) * np.nan
+        # median_vals = np.zeros_like(x_bins) * np.nan
+        count_vals = np.zeros_like(x_bins) * np.nan
+        lower = np.zeros_like(x_bins) * np.nan
+        upper = np.zeros_like(x_bins) * np.nan
+        vals_80_up = np.zeros_like(x_bins) * np.nan
+        vals_80_down = np.zeros_like(x_bins) * np.nan
+        # p_down_vals = np.zeros_like(x_bins) * np.nan
+
+        for x_idx, x_cent in enumerate(x_bins):
+
+            df_sub = df[ (df.x >= x_cent - x_radius) & (df.x <= x_cent + x_radius)]
+
+            if df_sub.shape[0]>min_vals:
+
+                # Get statistics via bootstrapping
+                n_bs = 40
+                boot_frac=1.0
+                med_array = np.zeros(n_bs)
+                for i_bs in range(n_bs):
+                    # Random subset the df
+                    df_rand = df_sub.sample(frac=boot_frac,replace=True)
+                    # med_array[i_bs] = np.median(df_rand.y) 
+                    med_array[i_bs] = np.mean(df_rand.y) 
+
+                # median_vals[x_idx] = np.nanmedian(df_sub.y) 
+                median_vals[x_idx] = np.mean(df_sub.y) 
+                vals_80_down[x_idx],vals_80_up[x_idx] = np.percentile(df_sub.y, [50+0.5*80.,50-0.5*80.])
+                # mean_vals[x_idx] = np.median(ratio_array)
+                count_vals[x_idx] = df_sub.shape[0]
+                # ci_vals[x_idx] = scipy.stats.sem(ratio_array, ddof=1) * 1.96 # df_sub.y.apply(lambda x: scipy.stats.sem(x, ddof=1) * 1.96)
+                # p_up_vals[x_idx] = p_up_func(ratio_array)# df_sub.y.apply(p_up_func)
+                # p_down_vals[x_idx] = p_down_func(ratio_array)#df_sub.y.apply(p_down_func)
+                # Get the confidence bounds
+                confidence=95
+                conf_bounds = [50+0.5*confidence,50-0.5*confidence]
+                # lower[x_idx], upper[x_idx] = (2*med_vals[x_idx]-np.percentile(med_array, conf_bounds))
+                lower[x_idx], upper[x_idx] = np.percentile(med_array, conf_bounds)
+
+
+        # # Plot the underlying points
+        if show_scatter:
+            ax.scatter(df['x'],df['y'],color=color,label='_nolegend_',alpha=1.0,s=35,marker='.')
+        if show_bin_points:
+            ax.scatter(x_bins,median_vals,color=color,s=count_vals,label='_nolegend_',alpha=0.6,marker='s')
+        if show_80:
+            ax.plot(x_bins,vals_80_down,'--',color=color,label='_nolegend_')
+            ax.plot(x_bins,vals_80_up,'--',color=color,label='_nolegend_')
+
+        # Plot the main trend
+        if not seaborn:
+            ax.plot(x_bins,median_vals,label=label,color=color)
+        else:
+            plt.plot(x_bins,median_vals,label=label,color=color)
+        
+        if show_confidence:
+            if not seaborn:
+                ax.fill_between(x_bins,lower,upper,alpha=0.2,color=color,label='_nolegend_')
+            else:
+                plt.fill_between(x_bins,lower,upper,alpha=0.2,color=color,label='_nolegend_')
+
+
+        return x_bins,median_vals,lower,upper
+
+    else:
+        ax.plot(0,0,label=label,color=color)
+
+        return np.nan,np.nan,np.nan, np.nan
