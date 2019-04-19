427bdd82a218f4789edebf7ffb02480774e68292
Eric Simley <eric.simley@nrel.gov>
2019-04-19 17:10:46 -0600

    Adding farm documentation

--- floris/simulation/farm.py
+++ floris/simulation/farm.py
@@ -22,38 +22,28 @@ import numpy as np
 class Farm():
     """
     Farm is the container class of the FLORIS package. It brings together all
-    of the component objects after input (ie Turbine, Wake, FlowField) and
+    of the component objects after input (i.e., Turbine, Wake, FlowField) and
     packages everything into the appropriate data type. Farm should also be used
     as an entry point to probe objects for generating output.
 
-    inputs:
-        instance_dictionary: dict - the input dictionary as generated from the input_reader;
+    Parameters:
+        instance_dictionary: A dictionary as generated from the input_reader;
             it should have the following key-value pairs:
                 {
-                    "wind_speed": float,
-
-                    "wind_direction": float,
-
-                    "turbulence_intensity": float,
-
-                    "wind_shear": float,
-
-                    "wind_veer": float,
-
-                    "air_density": float,
-
-                    "layout_x": list,
-
-                    "layout_y": list
-
+                    wind_speed: A float that is the wind speed at hub height (m/s).
+                    wind_direction: A float that is the wind direction (deg).
+                    turbulence_intensity: A float that is the turbulence intensity (expressed as a decimal fraction).
+                    wind_shear: A float that is the power law wind shear exponent.
+                    wind_veer: A float that is the vertical change in wind direction across the rotor.
+                    air_density: A float that is the air density (kg/m^3).
+                    layout_x: A list that contains the x coordinates of the turbines.
+                    layout_y: A list that contains the y coordinates of the turbines.
                 }
+        turbine: The Turbine object used in Farm.
+        wake: The Wake object used in Farm.
 
-        turbine: Turbine - Turbine instance used in Farm
-
-        wake: Wake - Wake instance used in Farm
-
-    outputs:
-        self: Farm - an instantiated Farm object
+    Returns:
+        An instantiated Farm object.
     """
 
     def __init__(self, instance_dictionary, turbine, wake):
@@ -86,21 +76,20 @@ class Farm():
 
     def set_wake_model(self, wake_model, calculate_wake=False):
         """
-        Sets the flow model
+        Sets the flow model.
 
-        inputs:
-        wake_model: string - the wake model used to calculate the wake;
-            valid wake model options are:
-                {
-                    "curl",
+        This method changes the wake model used. It will optionally calculate the new 
+        wake velocities and updates them with the flow field.
 
-                    "gauss",
+        Parameters:
+            wake_model: A string containing the wake model used to calculate the wake;
+            valid wake model options are: curl, gauss, jensen, floris
+            calculate_wake: A boolean that provides the option to calculate the wake after the wake model is set. 
+            This loops over the turbines, updating their velocities, calculating the wake deflection/deficit, 
+            and combines the wake with the flow field. 
 
-                    "jensen",
-
-                    "floris"
-                }
-        calculate_wake: boolean - option to calculate the wake after the wake model is set
+        Returns:
+            *None* -- The wake model and flow field are updated in the :py:obj:`floris.simulation.flow_field` object.
         """
 
         valid_wake_models = ['curl', 'gauss', 'jensen', 'floris']
@@ -127,15 +116,19 @@ class Farm():
 
     def set_yaw_angles(self, yaw_angles, calculate_wake=False):
         """
-        Sets yaw angles for all turbines and calculates the wake
+        This method sets yaw angles for all turbines and optionally calculates the new wake velocities 
+        and updates them with the flow field.
 
-        inputs:
-            yaw_angles:
-                float - constant yaw angle for all turbines in degrees
-                [float] - unique yaw angle for each turbine in degrees
-
-        outputs:
-            none
+        Parameters:
+            yaw_angles: A float of list of floats containing a constant yaw angle for all turbines 
+            or list of unique yaw angles for each turbine in degrees.
+            calculate_wake: A boolean that provides the option to calculate the wake after the wake model is set. 
+            This loops over the turbines, updating their velocities, calculating the wake deflection/deficit, 
+            and combines the wake with the flow field. 
+            
+        Returns:
+            *None* -- The turbines are updated directly and the flow field is updated in the 
+            :py:obj:`floris.simulation.flow_field` object.
         """
         if isinstance(yaw_angles, float) or isinstance(yaw_angles, int):
             yaw_angles = [yaw_angles] * len(self.turbines)
@@ -148,18 +141,21 @@ class Farm():
 
     def set_turbine_locations(self, layout_x, layout_y, calculate_wake=False):
         """
-        Sets the locations for all turbines and calculates the wake
+        This method sets the locations for all turbines and optionally calculates the wake velocities 
+        and updates them with the flow field.
 
-        inputs:
-            layout_x:
-                float - x coordinate(s) for the turbine(s)
-            
-            layout_y:
-                float - y coordinate(s) for the turbine(s)
+        Parameters:
+            layout_x: A list of floats that contains the x coordinate(s) for the turbine(s).
+            layout_y: A list of floats that contains the y coordinate(s) for the turbine(s).
+            calculate_wake: A boolean that provides the option to calculate the wake after the wake model is set. 
+            This loops over the turbines, updating their velocities, calculating the wake deflection/deficit, 
+            and combines the wake with the flow field.
 
-        outputs:
-            none
+        Returns:
+            *None* -- The turbine locations and flow field are updated in the 
+            :py:obj:`floris.simulation.flow_field` object.
         """
+
         # TODO: this function requires all turbines to be the same.
         # Is it reasonable to require the user to give a TurbineMap-like object
         # where the turbines are instantiated and located externally?
@@ -185,6 +181,24 @@ class Farm():
     # Getters & Setters
     @property
     def wind_speed(self):
+        """
+        This method gets or sets the wind speed.
+
+        Parameters:
+            wind_speed: A float that is the new wind speed for the wind farm in m/s.
+
+        Returns:
+            wind_speed: A float that is the current wind speed in the wind farm in m/s.
+
+        Examples:
+            To set the wind speed:
+            
+            >>> floris.farm.wind_speed(8.0)
+
+            To get the current wind speed:
+
+            >>> wind_speed = floris.farm.wind_speed()
+        """
         return self.flow_field.wind_speed
 
     @wind_speed.setter
@@ -193,6 +207,24 @@ class Farm():
 
     @property
     def wind_direction(self):
+        """
+        This method gets or sets the wind direction.
+
+        Parameters:
+            wind_direction: A float that is the new wind direction for the wind farm in degrees.
+
+        Returns:
+            wind_direction: A float that is the current wind direction in the wind farm in degrees.
+
+        Examples:
+            To set the wind direction:
+            
+            >>> floris.farm.wind_direction(90.0)
+
+            To get the current wind direction:
+
+            >>> wind_direction = floris.farm.wind_direction()
+        """
         return self.flow_field.wind_direction
 
     @wind_direction.setter
@@ -201,6 +233,24 @@ class Farm():
 
     @property
     def wind_shear(self):
+        """
+        This method gets or sets the wind shear power law exponent.
+
+        Parameters:
+            wind_shear: A float that is the new wind shear power law exponent for the wind farm.
+
+        Returns:
+            wind_shear: A float that is the current wind shear power law exponent in the wind farm.
+
+        Examples:
+            To set the wind shear:
+            
+            >>> floris.farm.wind_shear(0.14)
+
+            To get the current wind shear:
+
+            >>> wind_shear = floris.farm.wind_shear()
+        """
         return self.flow_field.wind_shear
 
     @wind_shear.setter
@@ -209,6 +259,24 @@ class Farm():
 
     @property
     def wind_veer(self):
+        """
+        This method gets or sets the wind veer -- the vertical change in wind direction across the rotor.
+
+        Parameters:
+            wind_veer: A float that is the new vertical change in wind direction across the rotor in degrees.
+
+        Returns:
+            wind_veer: A float that is the current vertical change in wind direction across the rotor in degrees.
+
+        Examples:
+            To set the wind veer:
+            
+            >>> floris.farm.wind_veer(20.0)
+
+            To get the current wind veer:
+
+            >>> wind_veer = floris.farm.wind_veer()
+        """
         return self.flow_field.wind_veer
 
     @wind_veer.setter
@@ -217,6 +285,24 @@ class Farm():
 
     @property
     def turbulence_intensity(self):
+        """
+        This method gets or sets the turbulence intensity.
+
+        Parameters:
+            turbulence_intensity: A float that is the new turbulence intensity expressed as a decimal fraction.
+
+        Returns:
+            turbulence_intensity: A float that is the current turbulence intensity expressed as a decimal fraction.
+
+        Examples:
+            To set the turbulence intensity:
+            
+            >>> floris.farm.turbulence_intensity(0.1)
+
+            To get the current turbulence intensity:
+
+            >>> turbulence_intensity = floris.farm.turbulence_intensity()
+        """
         return self.flow_field.turbulence_intensity
 
     @turbulence_intensity.setter
@@ -225,6 +311,24 @@ class Farm():
 
     @property
     def air_density(self):
+        """
+        This method gets or sets the air density.
+
+        Parameters:
+            air_density: A float that is the new air density in kg/m^3.
+
+        Returns:
+            air_density: A float that is the current air density in kg/m^3.
+
+        Examples:
+            To set the air density:
+            
+            >>> floris.farm.air_density(0.1)
+
+            To get the current air density:
+
+            >>> air_density = floris.farm.air_density()
+        """
         return self.flow_field.air_density
 
     @air_density.setter
@@ -233,6 +337,24 @@ class Farm():
 
     @property
     def turbine_map(self):
+        """
+        This method gets or sets the turbine map property of the :py:object:`floris.simulation.flow_field` object.
+
+        Parameters:
+            turbine_map: A :py:obj:`floris.simulation.turbine_map` object that holds turbine information for the farm.
+
+        Returns:
+            turbine_map: A :py:obj:`floris.simulation.turbine_map` object that holds turbine information for the farm.
+
+        Examples:
+            To set the turbine map:
+            
+            >>> floris.farm.turbine_map(turbine_map_new)
+
+            To get the current turbine map:
+
+            >>> turbine_map = floris.farm.turbine_map()
+        """
         return self.flow_field.turbine_map
     
     @turbine_map.setter
@@ -242,6 +364,9 @@ class Farm():
     @property
     def turbines(self):
         """
-        Returns a list of turbine objects gotten from the TurbineMap
+        This method returns the list of :py:obj:`floris.simulation.turbine` objects contained in the :py:obj:`floris.simulation.turbine_map` object.
+
+        Returns:
+            turbines: A list of :py:obj:`floris.simulation.turbine` objects that hold the turbine information.
         """
         return self.turbine_map.turbines
