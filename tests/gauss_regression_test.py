df0b3a8626ee0d9f7492a49266b322fae9bb29ee
Paul <paul.fleming@nrel.gov>
2019-06-13 11:06:09 -0600

    small unit test update

--- tests/gauss_regression_test.py
+++ tests/gauss_regression_test.py
@@ -39,7 +39,7 @@ class GaussRegressionTest():
 
     def yawed_baseline(self, turbine_index):
         baseline = [
-            (0.4632706, 0.7626695, 1780851.3400887, 0.2559061, 7.9736330),
+            (0.4632733, 0.7626695, 1780861.5909742, 0.2559061, 7.9736330),
             (0.4519389, 0.8403914, 696267.1270400, 0.3002448, 5.8647976)
         ]
         return baseline[turbine_index]
