b3060aa0b23c3590d97e2915f4ded2b1fbd84c5d
Paul <paul.fleming@nrel.gov>
2019-06-24 17:08:36 -0600

    Add optional comment line

--- floris/tools/cut_plane.py
+++ floris/tools/cut_plane.py
@@ -70,6 +70,7 @@ class _CutPlane():
         # Mesh and interpolate u, v and w
         self.x1_mesh, self.x2_mesh = np.meshgrid(self.x1_lin, self.x2_lin)
         self.u_mesh = griddata(
+            # np.column_stack([self.x1_in, self.x2_in]), # Uncomment to show issue
             np.column_stack([(1+np.finfo(float).eps) * self.x1_in, (1+np.finfo(float).eps) * self.x2_in]),
             self.u_in, (self.x1_mesh.flatten(), self.x2_mesh.flatten()),
             method='cubic')
