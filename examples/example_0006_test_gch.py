2e1d47d8405f2c5c04c53603a55c2d6b52ef7ed5
Paul <paul.fleming@nrel.gov>
2019-12-16 14:46:43 -0700

    add test gch example

---
+++ examples/example_0006_test_gch.py
@@ -0,0 +1,41 @@
+# Copyright 2019 NREL
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+# See read the https://floris.readthedocs.io for documentation
+
+import matplotlib.pyplot as plt
+import floris.tools as wfct
+from floris.utilities import Vec3
+
+# Initialize the FLORIS interface fi, use default gauss model
+fi = wfct.floris_utilities.FlorisInterface("example_input.json")
+
+# Change the layout
+D = fi.floris.farm.flow_field.turbine_map.turbines[0].rotor_diameter
+layout_x = [0, 7*D, 14*D]
+layout_y = [0, 0, 0]
+fi.reinitialize_flow_field(layout_array=(layout_x, layout_y))
+
+# Calculate wake
+fi.calculate_wake(yaw_angles=[25,0,0])
+
+# Print the turbine power
+print(fi.get_farm_power())
+
+# Switch to gch
+fi.floris.farm.wake._velocity_model.use_yaw_rec = True
+fi.floris.farm.wake._deflection_model.use_yaw_eff = True
+
+# Calculate wake
+fi.calculate_wake(yaw_angles=[25,0,0])
+
+# Print the turbine power
+print(fi.get_farm_power())
