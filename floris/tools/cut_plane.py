b3716851cbcee9e07ce6a31c0674d96da12e901f
Paul <paul.fleming@nrel.gov>
2020-01-08 11:19:05 -0700

    reverert to cubic

--- floris/tools/cut_plane.py
+++ floris/tools/cut_plane.py
@@ -161,16 +161,16 @@ def change_resolution(cut_plane, resolution=(100, 100)):
     u_mesh = griddata(
             np.column_stack([nudge_outward(cut_plane.df.x1), nudge_outward(cut_plane.df.x2)]),
             cut_plane.df.u.values, (x1_mesh.flatten(), x2_mesh.flatten()),
-            method='nearest')
+            method='cubic')
     v_mesh = griddata(
             np.column_stack([nudge_outward(cut_plane.df.x1), nudge_outward(cut_plane.df.x2)]),
             cut_plane.df.v.values, (x1_mesh.flatten(), x2_mesh.flatten()),
-            method='nearest')
+            method='cubic')
 
     w_mesh = griddata(
             np.column_stack([nudge_outward(cut_plane.df.x1), nudge_outward(cut_plane.df.x2)]),
             cut_plane.df.w.values, (x1_mesh.flatten(), x2_mesh.flatten()),
-            method='nearest')
+            method='cubic')
 
 
     # Assign back to df
