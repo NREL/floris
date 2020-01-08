a390fbe388861fd5233c1dcaf061ecf30c64a4be
Paul <paul.fleming@nrel.gov>
2020-01-08 11:20:05 -0700

    revert to cubic

--- floris/tools/cut_plane.py
+++ floris/tools/cut_plane.py
@@ -220,16 +220,16 @@ def interpolate_onto_array(cut_plane, x1_array, x2_array):
     u_mesh = griddata(
             np.column_stack([nudge_outward(cut_plane.df.x1), nudge_outward(cut_plane.df.x2)]),
             cut_plane.df.u.values, (x1_mesh.flatten(), x2_mesh.flatten()),
-            method='nearest')
+            method='cubic')
     v_mesh = griddata(
             np.column_stack([nudge_outward(cut_plane.df.x1), nudge_outward(cut_plane.df.x2)]),
             cut_plane.df.v.values, (x1_mesh.flatten(), x2_mesh.flatten()),
-            method='nearest')
+            method='cubic')
 
     w_mesh = griddata(
             np.column_stack([nudge_outward(cut_plane.df.x1), nudge_outward(cut_plane.df.x2)]),
             cut_plane.df.w.values, (x1_mesh.flatten(), x2_mesh.flatten()),
-            method='nearest')
+            method='cubic')
 
 
     # Assign back to df
