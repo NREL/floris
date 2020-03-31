b6e6c24be9a36f2bcb8c857d4c78560dc5724951
Paul <paul.fleming@nrel.gov>
2020-03-30 21:39:13 -0600

    include rotor point input

--- floris/simulation/turbine.py
+++ floris/simulation/turbine.py
@@ -94,6 +94,12 @@ class Turbine():
         self.air_density = -1
         self.use_turbulence_correction = False
 
+        # Initiate to False unless specifically set
+        if 'use_points_on_perimeter' in properties:
+            self.use_points_on_perimeter = bool(properties['use_points_on_perimeter'])
+        else:
+            self.use_points_on_perimeter = False
+
         self._initialize_turbine()
 
     # Private methods
@@ -147,10 +153,18 @@ class Turbine():
                 for h in horizontal]
 
         # keep only the points in the swept area
-        grid = [
-            point for point in grid
-            if np.hypot(point[0], point[1]) < self.rotor_radius
-        ]
+        if self.use_points_on_perimeter:
+            print('<=')
+            grid = [
+                point for point in grid
+                if np.hypot(point[0], point[1]) <= self.rotor_radius
+            ]
+        else:
+            print('<')
+            grid = [
+                point for point in grid
+                if np.hypot(point[0], point[1]) < self.rotor_radius
+            ]
 
         return grid
 
