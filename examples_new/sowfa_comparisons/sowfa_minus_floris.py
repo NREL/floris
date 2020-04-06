1bbe1357a974118727f6acc4d184e17fd197c0a6
Paul <paul.fleming@nrel.gov>
2020-04-06 15:12:57 -0600

    delete commented code

--- examples_new/sowfa_comparisons/sowfa_minus_floris.py
+++ examples_new/sowfa_comparisons/sowfa_minus_floris.py
@@ -94,47 +94,5 @@ wfct.visualization.visualize_cut_plane(
 )
 ax.set_title('SOWFA - FLORIS Projected')
 
-# # Plot the SOWFA flow and turbines using the input information
-# fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(5, 8.5))
-
-# hor_plane = si.get_hor_plane(90)
-# wfct.visualization.visualize_cut_plane(
-#     hor_plane, ax=ax2, minSpeed=minspeed, maxSpeed=maxspeed)
-# vis.plot_turbines(ax2, si.layout_x, si.layout_y, si.yaw_angles, si.D)
-# ax2.set_title('SOWFA')
-# ax2.set_ylabel('y location [m]')
-
-# # floris_flow_data_orig = fi.get_flow_data()
-
-# # Plot the original FLORIS flow and turbines using the input information
-# hor_plane_orig = fi.get_hor_plane(90)
-# wfct.visualization.visualize_cut_plane(
-#     hor_plane_orig, ax=ax1, minSpeed=minspeed, maxSpeed=maxspeed)
-# vis.plot_turbines(ax1, fi.layout_x, fi.layout_y,
-#                   fi.get_yaw_angles(),
-#                   fi.floris.farm.turbine_map.turbines[0].rotor_diameter)
-# ax1.set_title('FLORIS - Original')
-# ax1.set_ylabel('y location [m]')
-
-# # Set the relevant FLORIS parameters to equal the SOWFA case
-# fi.reinitialize_flow_field(wind_speed=[si.precursor_wind_speed],
-#                            wind_direction=[si.precursor_wind_dir],
-#                            layout_array=(si.layout_x, si.layout_y)
-#                            )
-
-# # Set the yaw angles
-# fi.calculate_wake(yaw_angles=si.yaw_angles)
-
-# # Plot the FLORIS flow and turbines using the input information, and bounds from SOWFA
-# hor_plane_matched = fi.get_hor_plane(90,x_bounds=[np.min(sowfa_flow_data.x), np.max(sowfa_flow_data.x)],
-#                                         y_bounds=[np.min(sowfa_flow_data.y), np.max(sowfa_flow_data.y)])
-# wfct.visualization.visualize_cut_plane(
-#     hor_plane_matched, ax=ax3, minSpeed=minspeed, maxSpeed=maxspeed)
-# vis.plot_turbines(ax3, fi.layout_x, fi.layout_y,
-#                   fi.get_yaw_angles(),
-#                   fi.floris.farm.turbine_map.turbines[0].rotor_diameter)
-# ax3.set_title('FLORIS - Matched')
-# ax3.set_xlabel('x location [m]')
-# ax3.set_ylabel('y location [m]')
 
 plt.show()
