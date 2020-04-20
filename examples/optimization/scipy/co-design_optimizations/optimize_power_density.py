000750766efc2159647a1354a660cdce0b875650
Paul <paul.fleming@nrel.gov>
2020-04-20 17:04:27 -0600

    Update copyright statement

--- examples/optimization/scipy/co-design_optimizations/optimize_power_density.py
+++ examples/optimization/scipy/co-design_optimizations/optimize_power_density.py
@@ -1,16 +1,17 @@
 # Copyright 2020 NREL
-
+ 
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
 # the License at http://www.apache.org/licenses/LICENSE-2.0
-
+ 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-
-# See read the https://floris.readthedocs.io for documentation
+ 
+# See https://floris.readthedocs.io for documentation
+ 
 
 # This example optimization takes a 2x2 array of turbines and works to reduce
 # the wind farm footprint area (measured as a convex hull), while working to
