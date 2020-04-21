81d9469076ddb40341474a749e8296bfa872e4df
Rafael M Mudafort <rafmudaf@gmail.com>
2020-04-21 11:44:33 -0500

    Include init-function docstrings in build

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ autosummary_generate = True
 
 # Napoleon settings
 napoleon_google_docstring = True
+napoleon_include_init_with_doc = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
