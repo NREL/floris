2385a5cee61ddb2cbbe6d522f8ed24b55ec2d2ee
bayc <christopher.j.bay@gmail.com>
2021-01-13 13:52:49 -0700

    Updating comments in example

--- examples/visualization/interactive_visual.py
+++ examples/visualization/interactive_visual.py
@@ -1,9 +1,27 @@
+# Copyright 2020 NREL
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+# See https://floris.readthedocs.io for documentation
+
+
+# NOTE: This example requires an additional module, `streamlit`.
+#
 # TO USE THIS FROM COMMAND TYPE:
 #    streamlit run interactive_visual.py
 
+import matplotlib.pyplot as plt
+
 import streamlit as st
 import floris.tools as wfct
-import matplotlib.pyplot as plt
 
 
 # Fixed parameters
