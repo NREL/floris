3d59bca6a926c3702190e23668133f200142ff3f
King <jennifer.king@nrel.gov>
2021-02-10 21:32:49 -0700

    Changed vertical deflection to depend on vertical velocity rather than spanwise

--- examples/vertical_deflection/vertical_deflection.py
+++ examples/vertical_deflection/vertical_deflection.py
@@ -29,6 +29,7 @@ layout_y = [0, 0, 0]
 fi.reinitialize_flow_field(layout_array=(layout_x,layout_y))
 fi.calculate_wake()
 power_init = fi.get_farm_power()
+print(fi.get_turbine_power())
 
 # Get horizontal plane at default height (hub-height)
 hor_plane = fi.get_hor_plane()
@@ -39,17 +40,11 @@ wfct.visualization.visualize_cut_plane(hor_plane, ax=ax)
 
 # change the tilt angle
 fi.reinitialize_flow_field()
-fi.calculate_wake(yaw_angles=[0.0, 0.0, 0.0], tilt_angles=[-25.0, 0.0, 0.0])
+fi.calculate_wake(yaw_angles=[0.0, 0.0, 0.0], tilt_angles=[25.0, 0.0, 0.0])
 power_tilt = fi.get_farm_power()
+print(fi.get_turbine_power())
 
 print('Power difference = ', 100 * (power_tilt - power_init) / power_init)
-
-# Get horizontal plane at default height (hub-height)
-hor_plane = fi.get_hor_plane()
-
-# Plot and show
-fig, ax = plt.subplots()
-wfct.visualization.visualize_cut_plane(hor_plane, ax=ax)
 
 # Plot and show
 y_plane = fi.get_y_plane(0.0)
