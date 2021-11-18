7adbd267366c4e309da74d7e0e6e4561a76eb9cf
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2021-11-18 14:36:31 -0700

    adding tests

--- tests/floris_interface_test.py
+++ tests/floris_interface_test.py
@@ -14,6 +14,137 @@ def test_read_json():
 
 
 def test_read_yaml():
-    print(YAML_INPUT)
     fi = FlorisInterface(configuration=YAML_INPUT)
     assert isinstance(fi, FlorisInterface)
+
+
+def test_calculate_wake():
+    pass
+
+
+def test_reinitialize_flow_field():
+    pass
+
+
+def test_get_plane_of_points():
+    pass
+
+
+def test_get_set_of_points():
+    pass
+
+
+def test_get_hor_plane():
+    pass
+
+
+def test_get_cross_plane():
+    pass
+
+
+def test_get_y_plane():
+    pass
+
+
+def test_get_flow_data():
+    pass
+
+
+def test_get_yaw_angles():
+    pass
+
+
+def test_get_farm_power():
+    pass
+
+
+def test_get_turbine_layout():
+    pass
+
+
+def test_get_power_curve():
+    pass
+
+
+def test_get_turbine_ct():
+    pass
+
+
+def test_get_turbine_ti():
+    pass
+
+
+def test_get_farm_power_for_yaw_angle():
+    pass
+
+
+def test_get_farm_AEP():
+    pass
+
+
+def test_calc_one_AEP_case():
+    pass
+
+
+def test_get_farm_AEP_parallel():
+    pass
+
+
+def test_calc_AEP_wind_limit():
+    pass
+
+
+def test_calc_change_turbine():
+    pass
+
+
+def test_set_use_points_on_perimeter():
+    pass
+
+
+def test_set_gch():
+    pass
+
+
+def test_set_gch_yaw_added_recovery():
+    pass
+
+
+def test_set_gch_secondary_steering():
+    pass
+
+
+def test_layout_x():  # TODO
+    pass
+
+
+def test_layout_y():  # TODO
+    pass
+
+
+def test_TKE_to_TI():
+    pass
+
+
+def test_set_rotor_diameter():  # TODO
+    pass
+
+
+def test_show_model_parameters():  # TODO
+    pass
+
+
+def test_get_model_parameters():  # TODO
+    pass
+
+
+def test_set_model_parameters():  # TODO
+    pass
+
+
+def test_vis_layout():
+    pass
+
+
+def test_show_flow_field():
+    pass
