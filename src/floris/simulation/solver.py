2993b70cf529731f8e27a2607b1538557486a158
Rafael M Mudafort <rafmudaf@gmail.com>
2021-12-16 01:32:16 -0600

    Reenable the full flow field solve
    Temporarily removed the WAT calculation

--- src/floris/simulation/solver.py
+++ src/floris/simulation/solver.py
@@ -167,29 +167,18 @@ def full_flow_sequential_solver(farm: Farm, flow_field: FlowField, grid: FlowFie
     turbine_turbulence_intensity = 0.1 * np.ones_like(grid.x)
     ambient_turbulence_intensity = 0.1 * np.ones_like(grid.x)
 
-    reference_rotor_diameter = farm.reference_turbine_diameter * np.ones(
-        (
-            flow_field.n_wind_directions,
-            flow_field.n_wind_speeds,
-            *grid.grid_resolution
-        )
-    )
+    reference_rotor_diameter = farm.reference_turbine_diameter
 
     for i in range(grid.n_turbines):
-        x_i = np.mean(turbine_grid.x[:, :, i:i+1], axis=(3, 4))
-        x_i = x_i[:, :, :, None, None]
-
-        y_i = np.mean(turbine_grid.y[:, :, i:i+1], axis=(3, 4))        
-        y_i = y_i[:, :, :, None, None]
-
-        z_i = np.mean(turbine_grid.z[:, :, i:i+1], axis=(3, 4))
-        z_i = z_i[:, :, :, None, None]
+        x_i = farm.coordinates[i].x1
+        y_i = farm.coordinates[i].x2
+        z_i = farm.coordinates[i].x3
 
         u = flow_field.u_initial - wake_field
 
         ct_i = Ct(
             velocities=u,
-            yaw_angle=farm.farm_controller.yaw_angles,
+            yaw_angle=farm.yaw_angles,
             fCt=farm.fCt_interp,
             ix_filter=[i],
         )
@@ -197,14 +186,14 @@ def full_flow_sequential_solver(farm: Farm, flow_field: FlowField, grid: FlowFie
 
         axial_induction_i = axial_induction(
             velocities=u,
-            yaw_angle=farm.farm_controller.yaw_angles,
+            yaw_angle=farm.yaw_angles,
             fCt=farm.fCt_interp,
             ix_filter=[i],
         )
         axial_induction_i = axial_induction_i[:, :, :, None, None]
 
         turbulence_intensity_i = turbine_turbulence_intensity[:, :, i:i+1]
-        yaw_i = farm.farm_controller.yaw_angles[:, :, i:i+1, None, None]
+        yaw_i = farm.yaw_angles[:, :, i:i+1, None, None]
 
         deflection_field = model_manager.deflection_model.function(
             x_i,
@@ -243,20 +232,22 @@ def full_flow_sequential_solver(farm: Farm, flow_field: FlowField, grid: FlowFie
         # area_overlap = calculate_area_overlap(
         #     turb_wake_field, flow_field.u_initial, 5, 5
         # )
-        area_overlap = np.sum(velocity_deficit * flow_field.u_initial > 0.05, axis=(3, 4)) / (turbine_grid.grid_resolution * turbine_grid.grid_resolution)
-        area_overlap = area_overlap[:, :, :, None, None]
-
+        # print(np.shape(flow_field.u_initial))
+        # area_overlap = np.sum(velocity_deficit * flow_field.u_initial > 0.05, axis=(3, 4)) / (turbine_grid.grid_resolution * turbine_grid.grid_resolution)
+        # area_overlap = area_overlap[:, :, :, None, None]
+        # # print(area_overlap)
+        # print(np.shape(area_overlap))
         # Modify wake added turbulence by wake area overlap
-        downstream_influence_length = 15 * reference_rotor_diameter
-        ti_added = (
-            area_overlap
-            * np.nan_to_num(wake_added_turbulence_intensity, posinf=0.0)
-            * np.array(grid.x > x_i)
-            * np.array(np.abs(y_i - grid.y) < 2 * reference_rotor_diameter)
-            * np.array(grid.x <= downstream_influence_length + x_i)
-        )
+        # downstream_influence_length = 15 * reference_rotor_diameter
+        # ti_added = (
+        #     area_overlap
+        #     * np.nan_to_num(wake_added_turbulence_intensity, posinf=0.0)
+        #     * np.array(grid.x > x_i)
+        #     * np.array(np.abs(y_i - grid.y) < 2 * reference_rotor_diameter)
+        #     * np.array(grid.x <= downstream_influence_length + x_i)
+        # )
 
         # Combine turbine TIs with WAT
-        turbine_turbulence_intensity = np.maximum( np.sqrt( ti_added ** 2 + ambient_turbulence_intensity ** 2 ) , turbine_turbulence_intensity )
+        # turbine_turbulence_intensity = np.maximum( np.sqrt( ti_added ** 2 + ambient_turbulence_intensity ** 2 ) , turbine_turbulence_intensity )
 
     flow_field.u = flow_field.u_initial - wake_field
