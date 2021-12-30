432a125dd1022d946f8e2cb331b94a93e6d7352e
Rafael M Mudafort <rafmudaf@gmail.com>
2021-12-30 04:48:44 -0500

    Rotor plane plotting: allow to select dimensions

--- src/floris/tools/visualization.py
+++ src/floris/tools/visualization.py
@@ -19,6 +19,7 @@ from itertools import product
 
 import numpy as np
 import matplotlib as mpl
+import matplotlib.colors as mplcolors
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
@@ -210,11 +211,13 @@ def reverse_cut_plane_x_axis_in_plot(ax):
 
 def plot_rotor_values(
     values: np.ndarray,
-    titles: np.ndarray = None,
-    max_width: int = 4,
+    wd_range = None,
+    ws_range = None,
+    t_range = None,
     cmap: str = "coolwarm",
     return_fig_objects: bool = False,
     save_path: Union[str, None] = None,
+    show = False
 ) -> Union[None, tuple[plt.figure, plt.axes]]:
     """Plots the gridded turbine rotor values. This is intended to be used for
     understanding the differences between two sets of values, so each subplot can be
@@ -223,9 +226,6 @@ def plot_rotor_values(
     Parameters:
         values (np.ndarray): The 5-dimensional array of values to plot. Should be:
             N wind directions x N wind speeds x N turbines X N rotor points X N rotor points.
-        titles (None | np.ndarray): The string values to label each plot, and should be of the
-            same shape as `values`, by default None.
-        max_width (int): The maximum number of subplots in one row, default 4.
         cmap (str): The matplotlib colormap to be used, default "coolwarm".
         return_fig_objects (bool): Indicator to return the primary figure objects for
             further editing, default False.
@@ -239,58 +239,49 @@ def plot_rotor_values(
 
     cmap = plt.cm.get_cmap(name=cmap)
 
-    n_wd, n_ws, n_turb, *_ = values.shape
+    if wd_range is None:
+        wd_range = range(values.shape[0])
+    if ws_range is None:
+        ws_range = range(values.shape[1])
+    if t_range is None:
+        t_range = range(values.shape[2])
 
-    vmin = values.min()
-    vmax = values.max()
+    n_wd = len(wd_range)
+    n_ws = len(ws_range)
+    n_turb = len(t_range)
 
-    rounded_vmin = round(math.floor(vmin) * 2) / 2
-    if vmin % 1 >= 0.5:
-        rounded_vmin += 0.5
+    ncols = n_turb
+    nrows = n_wd * n_ws
 
-    rounded_vmax = round(math.ceil(vmax) * 2) / 2
-    if vmax % 1 <= 0.5:
-        rounded_vmax -= 0.5
-
-    bounds = np.linspace(rounded_vmin, rounded_vmax, 61)
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-
-    n_plots = n_wd * n_ws
-    extra = 0
-    if n_plots <= max_width:
-        nrows, ncols = 1, n_plots
-    else:
-        nrows, extra = divmod(n_plots, max_width)
-        if extra > 0:
-            nrows += 1
-            extra = max_width - extra
-        ncols = max_width
-
-    fig = plt.figure(dpi=200, figsize=(16, 16))
+    fig = plt.figure()
     axes = fig.subplots(nrows, ncols)
 
-    # Create the title values if they aren't already existing
-    indices = list(product(range(n_wd), range(n_ws), range(n_turb)))
-    if titles is None:
-        titles = np.array([f"Wind Direction: {i}\nWind Speed: {j}\n Turbine: {k}" for i, j, k in indices])
-        titles = titles.reshape(n_wd, n_ws, n_turb)
+    indices = list(product(wd_range, ws_range, t_range))
+    titles = np.array([f"[{i},{j},{k}]" for i, j, k in indices])
+    titles = titles.reshape(n_wd, n_ws, n_turb)
 
     for ax, t, (i, j, k) in zip(axes.flatten(), titles.flatten(), indices):
-        ax.imshow(values[i, j, k], cmap=cmap, norm=norm)
+
+        vmin = np.min(values[i, j])
+        vmax = np.max(values[i, j])
+
+        bounds = np.linspace(vmin, vmax, 61)
+        norm = mplcolors.BoundaryNorm(bounds, cmap.N)
+
+        ax.imshow(values[i, j, k].T, cmap=cmap, norm=norm, origin="lower")
 
         ax.set_xticks([])
         ax.set_yticks([])
         ax.set_title(t)
 
-    if extra > 0:
-        for ax in axes[-1][-extra:]:
-            fig.delaxes(ax)
-
     cbar_ax = fig.add_axes([0.05, 0.125, 0.03, 0.75])
     cb = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=cbar_ax)
 
-    if save_path is not None:
+    if save_path:
         plt.savefig(save_path, bbox_inches="tight")
 
     if return_fig_objects:
         return fig, axes, cbar_ax, cb
+
+    if show:
+        plt.show()
