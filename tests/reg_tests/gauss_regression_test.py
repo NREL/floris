e15c3ae9a2f86112f521601c66c265782406077a
Rafael M Mudafort <rafmudaf@gmail.com>
2022-01-03 16:11:11 -0500

    Update Gauss regression test baselines for v3
    The results did change very slightly for the cases with GCH components enabled. This could be due to a difference in the method for determining the effective yaw angle. However, this difference has been noted in GitHub issues and should be evaluated further.
    https://github.com/rafmudaf/floris/issues/50

--- tests/reg_tests/gauss_regression_test.py
+++ tests/reg_tests/gauss_regression_test.py
@@ -80,23 +80,155 @@ yawed_baseline = np.array(
     ]
 )
 
-# gch_baseline = [
-#     (0.7626396, 1802595.0749161, 0.2558908, 7.9803783),
-#     (0.8392733, 720745.6467388, 0.2995463, 5.8888694),
-#     (0.8354140, 755306.6488337, 0.2971540, 5.9719556),
-# ]
+"""
+# These are the results from v2.4 develop branch
+gch_baseline = np.array(
+    [
+        # 8 m/s
+        [
+            [7.9803783, 0.7605249, 1683956.3885389, 0.2548147],
+            [5.8920347, 0.8409478, 669953.8921404, 0.3005933],
+            [5.9690770, 0.8370054, 696678.9863587, 0.2981370],
+        ],
+        # 9 m/s
+        [
+            [8.9779256, 0.7596713, 2397237.3791443, 0.2543815],
+            [6.6299831, 0.8071465, 970496.1338006, 0.2804246],
+            [6.7527627, 0.8022061, 1027643.3724351, 0.2776299],
+        ],
+        # 10 m/s
+        [
+            [9.9754729, 0.7499157, 3283592.6005045, 0.2494847],
+            [7.3852773, 0.7796129, 1346745.9407360, 0.2652730],
+            [7.5343901, 0.7749587, 1428106.9252795, 0.2628074],
+        ],
+        # 11 m/s
+        [
+            [10.9730201, 0.7276532, 4344217.6993801, 0.2386508],
+            [8.1727131, 0.7624523, 1824643.2726943, 0.2563057],
+            [8.2996789, 0.7621064, 1911032.3885037, 0.2561283],
+        ]
+    ]
+)
 
-# yaw_added_recovery_baseline = [
-#     (0.7626396, 1802595.0749161, 0.2558908, 7.9803783),
-#     (0.8392735, 720743.5433910, 0.2995465, 5.8888643),
-#     (0.8356813, 752912.7537170, 0.2973188, 5.9662006),
-# ]
+secondary_steering_baseline = np.array(
+    [
+        # 8 m/s
+        [
+            [7.9803783, 0.7605249, 1683956.3885389, 0.2548147],
+            [5.8728752, 0.8419282, 663307.6815433, 0.3012088],
+            [5.9488299, 0.8380415, 689655.4839532, 0.2987797],
+        ],
+        # 9 m/s
+        [
+            [8.9779256, 0.7596713, 2397237.3791443, 0.2543815],
+            [6.6084854, 0.8080115, 960490.1060497, 0.2809176],
+            [6.7305708, 0.8030991, 1017314.2281904, 0.2781324],
+        ],
+        # 10 m/s
+        [
+            [9.9754729, 0.7499157, 3283592.6005045, 0.2494847],
+            [7.3621072, 0.7803734, 1334476.0326665, 0.2656783],
+            [7.5106613, 0.7755721, 1413887.2753700, 0.2631309],
+        ],
+        # 11 m/s
+        [
+            [10.9730201, 0.7276532, 4344217.6993801, 0.2386508],
+            [8.1489930, 0.7625169, 1808503.8150366, 0.2563388],
+            [8.2759469, 0.7621711, 1894884.8361479, 0.2561615],
+        ]
+    ]
+)
+"""
 
-# secondary_steering_baseline = [
-#     (0.7626396, 1802595.0749161, 0.2558908, 7.9803783),
-#     (0.8401691, 712723.9446334, 0.3001057, 5.8695848),
-#     (0.8363526, 746901.3150465, 0.2977332, 5.9517488),
-# ]
+
+gch_baseline = np.array(
+    [
+        # 8 m/s
+        [
+            [7.9803784, 0.7605250, 1683956.4331016, 0.2548147],
+            [5.8728717, 0.8418841, 663239.2727837, 0.3011691],
+            [5.9431088, 0.8383165, 687643.6142763, 0.2989459],
+        ],
+        # 9 m/s
+        [
+            [8.9779257, 0.7596713, 2397237.5549533, 0.2543816],
+            [6.6084816, 0.8079774, 960406.6413859, 0.2808905],
+            [6.7241672, 0.8033432, 1014300.6959592, 0.2782671],
+        ],
+        # 10 m/s
+        [
+            [9.9754729, 0.7499157, 3283592.6816183, 0.2494847],
+            [7.3621029, 0.7803471, 1334391.0196529, 0.2656592],
+            [7.5036655, 0.7757424, 1409656.9601407, 0.2632188],
+        ],
+        # 11 m/s
+        [
+            [10.9730203, 0.7276532, 4344217.9900289, 0.2386508],
+            [8.1489889, 0.7624968, 1808409.0900146, 0.2563249],
+            [8.2684705, 0.7621832, 1889759.5240748, 0.2561662],
+        ],
+    ]
+)
+
+yaw_added_recovery_baseline = np.array(
+    [
+        # 8 m/s
+        [
+            [7.9803784, 0.7605250, 1683956.4331016, 0.2548147],
+            [5.8728717, 0.8419284, 663306.4634311, 0.3012089],
+            [5.9429708, 0.8383413, 687623.0405897, 0.2989660],
+        ],
+        # 9 m/s
+        [
+            [8.9779257, 0.7596713, 2397237.5549533, 0.2543816],
+            [6.6084816, 0.8080117, 960488.3771712, 0.2809176],
+            [6.7240665, 0.8033608, 1014286.8885931, 0.2782799],
+        ],
+        # 10 m/s
+        [
+            [9.9754729, 0.7499157, 3283592.6816183, 0.2494847],
+            [7.3621029, 0.7803735, 1334473.8489294, 0.2656784],
+            [7.5035933, 0.7757548, 1409651.8187149, 0.2632273],
+        ],
+        # 11 m/s
+        [
+            [10.9730203, 0.7276532, 4344217.9900289, 0.2386508],
+            [8.1489889, 0.7625170, 1808500.9995864, 0.2563388],
+            [8.2684177, 0.7621916, 1889761.9109976, 0.2561720],
+        ],
+    ]
+)
+
+secondary_steering_baseline = np.array(
+    [
+        # 8 m/s
+        [
+            [7.9803784, 0.7605250, 1683956.4331016, 0.2548147],
+            [5.8728717, 0.8418841, 663239.2727837, 0.3011691],
+            [5.9431088, 0.8383165, 687643.6142763, 0.2989459],
+        ],
+        # 9 m/s
+        [
+            [8.9779257, 0.7596713, 2397237.5549533, 0.2543816],
+            [6.6084816, 0.8079774, 960406.6413859, 0.2808905],
+            [6.7241672, 0.8033432, 1014300.6959592, 0.2782671],
+        ],
+        # 10 m/s
+        [
+            [9.9754729, 0.7499157, 3283592.6816183, 0.2494847],
+            [7.3621029, 0.7803471, 1334391.0196529, 0.2656592],
+            [7.5036655, 0.7757424, 1409656.9601407, 0.2632188],
+        ],
+        # 11 m/s
+        [
+            [10.9730203, 0.7276532, 4344217.9900289, 0.2386508],
+            [8.1489889, 0.7624968, 1808409.0900146, 0.2563249],
+            [8.2684705, 0.7621832, 1889759.5240748, 0.2561662],
+        ]
+    ]
+)
+
 
 # Note: compare the yawed vs non-yawed results. The upstream turbine
 # power should be lower in the yawed case. The following turbine
@@ -298,3 +430,263 @@ def test_regression_yaw(sample_inputs_fixture):
         )
 
     assert_results_arrays(test_results[0], yawed_baseline)
+
+
+def test_regression_gch(sample_inputs_fixture):
+    """
+    Tandem turbines with the upstream turbine yawed, yaw added recovery
+    correction enabled, and secondary steering enabled
+    """
+    sample_inputs_fixture.floris["wake"]["model_strings"]["velocity_model"] = VELOCITY_MODEL
+    sample_inputs_fixture.floris["wake"]["model_strings"]["deflection_model"] = DEFLECTION_MODEL
+
+    ### With GCH off (via conftest), GCH should be same as Gauss
+
+    floris = Floris(sample_inputs_fixture.floris)
+
+    yaw_angles = np.zeros((N_WIND_DIRECTIONS, N_WIND_SPEEDS, N_TURBINES))
+    yaw_angles[:,:,0] = 5.0
+    floris.farm.yaw_angles = yaw_angles
+    
+    floris.steady_state_atmospheric_condition()
+
+    n_turbines = floris.farm.n_turbines
+    n_wind_speeds = floris.flow_field.n_wind_speeds
+    n_wind_directions = floris.flow_field.n_wind_directions
+
+    velocities = floris.flow_field.u
+    yaw_angles = floris.farm.yaw_angles
+    test_results = np.zeros((n_wind_directions, n_wind_speeds, n_turbines, 4))
+
+    farm_avg_velocities = average_velocity(
+        velocities,
+    )
+    farm_cts = Ct(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    farm_powers = power(
+        floris.flow_field.air_density,
+        velocities,
+        yaw_angles,
+        floris.farm.pP,
+        floris.farm.power_interp,
+    )
+    farm_axial_inductions = axial_induction(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    for i in range(n_wind_directions):
+        for j in range(n_wind_speeds):
+            for k in range(n_turbines):
+                test_results[i, j, k, 0] = farm_avg_velocities[i, j, k]
+                test_results[i, j, k, 1] = farm_cts[i, j, k]
+                test_results[i, j, k, 2] = farm_powers[i, j, k]
+                test_results[i, j, k, 3] = farm_axial_inductions[i, j, k]
+
+    # if DEBUG:
+    #     print_test_values(
+    #         farm_avg_velocities,
+    #         farm_cts,
+    #         farm_powers,
+    #         farm_axial_inductions,
+    #     )
+
+    assert_results_arrays(test_results[0], yawed_baseline)
+
+
+    ### With GCH on, the results should change
+    sample_inputs_fixture.floris["wake"]["enable_transverse_velocities"] = True
+    sample_inputs_fixture.floris["wake"]["enable_secondary_steering"] = True
+    sample_inputs_fixture.floris["wake"]["enable_yaw_added_recovery"] = True
+
+    floris = Floris(sample_inputs_fixture.floris)
+
+    yaw_angles = np.zeros((N_WIND_DIRECTIONS, N_WIND_SPEEDS, N_TURBINES))
+    yaw_angles[:,:,0] = 5.0
+    floris.farm.yaw_angles = yaw_angles
+    
+    floris.steady_state_atmospheric_condition()
+
+    n_turbines = floris.farm.n_turbines
+    n_wind_speeds = floris.flow_field.n_wind_speeds
+    n_wind_directions = floris.flow_field.n_wind_directions
+
+    velocities = floris.flow_field.u
+    yaw_angles = floris.farm.yaw_angles
+    test_results = np.zeros((n_wind_directions, n_wind_speeds, n_turbines, 4))
+
+    farm_avg_velocities = average_velocity(
+        velocities,
+    )
+    farm_cts = Ct(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    farm_powers = power(
+        floris.flow_field.air_density,
+        velocities,
+        yaw_angles,
+        floris.farm.pP,
+        floris.farm.power_interp,
+    )
+    farm_axial_inductions = axial_induction(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    for i in range(n_wind_directions):
+        for j in range(n_wind_speeds):
+            for k in range(n_turbines):
+                test_results[i, j, k, 0] = farm_avg_velocities[i, j, k]
+                test_results[i, j, k, 1] = farm_cts[i, j, k]
+                test_results[i, j, k, 2] = farm_powers[i, j, k]
+                test_results[i, j, k, 3] = farm_axial_inductions[i, j, k]
+
+    if DEBUG:
+        print_test_values(
+            farm_avg_velocities,
+            farm_cts,
+            farm_powers,
+            farm_axial_inductions,
+        )
+
+    assert_results_arrays(test_results[0], gch_baseline)
+
+
+def test_regression_yaw_added_recovery(sample_inputs_fixture):
+    """
+    Tandem turbines with the upstream turbine yawed and yaw added recovery
+    correction enabled
+    """
+
+    sample_inputs_fixture.floris["wake"]["model_strings"]["velocity_model"] = VELOCITY_MODEL
+    sample_inputs_fixture.floris["wake"]["model_strings"]["deflection_model"] = DEFLECTION_MODEL
+
+    sample_inputs_fixture.floris["wake"]["enable_transverse_velocities"] = True
+    sample_inputs_fixture.floris["wake"]["enable_secondary_steering"] = False
+    sample_inputs_fixture.floris["wake"]["enable_yaw_added_recovery"] = True
+
+    floris = Floris(sample_inputs_fixture.floris)
+
+    yaw_angles = np.zeros((N_WIND_DIRECTIONS, N_WIND_SPEEDS, N_TURBINES))
+    yaw_angles[:,:,0] = 5.0
+    floris.farm.yaw_angles = yaw_angles
+    
+    floris.steady_state_atmospheric_condition()
+
+    n_turbines = floris.farm.n_turbines
+    n_wind_speeds = floris.flow_field.n_wind_speeds
+    n_wind_directions = floris.flow_field.n_wind_directions
+
+    velocities = floris.flow_field.u
+    yaw_angles = floris.farm.yaw_angles
+    test_results = np.zeros((n_wind_directions, n_wind_speeds, n_turbines, 4))
+
+    farm_avg_velocities = average_velocity(
+        velocities,
+    )
+    farm_cts = Ct(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    farm_powers = power(
+        floris.flow_field.air_density,
+        velocities,
+        yaw_angles,
+        floris.farm.pP,
+        floris.farm.power_interp,
+    )
+    farm_axial_inductions = axial_induction(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    for i in range(n_wind_directions):
+        for j in range(n_wind_speeds):
+            for k in range(n_turbines):
+                test_results[i, j, k, 0] = farm_avg_velocities[i, j, k]
+                test_results[i, j, k, 1] = farm_cts[i, j, k]
+                test_results[i, j, k, 2] = farm_powers[i, j, k]
+                test_results[i, j, k, 3] = farm_axial_inductions[i, j, k]
+
+    if DEBUG:
+        print_test_values(
+            farm_avg_velocities,
+            farm_cts,
+            farm_powers,
+            farm_axial_inductions,
+        )
+
+    assert_results_arrays(test_results[0], yaw_added_recovery_baseline)
+
+
+def test_regression_secondary_steering(sample_inputs_fixture):
+    """
+    Tandem turbines with the upstream turbine yawed and secondary steering enabled
+    """
+
+    sample_inputs_fixture.floris["wake"]["model_strings"]["velocity_model"] = VELOCITY_MODEL
+    sample_inputs_fixture.floris["wake"]["model_strings"]["deflection_model"] = DEFLECTION_MODEL
+
+    sample_inputs_fixture.floris["wake"]["enable_transverse_velocities"] = True
+    sample_inputs_fixture.floris["wake"]["enable_secondary_steering"] = True
+    sample_inputs_fixture.floris["wake"]["enable_yaw_added_recovery"] = False
+
+    floris = Floris(sample_inputs_fixture.floris)
+
+    yaw_angles = np.zeros((N_WIND_DIRECTIONS, N_WIND_SPEEDS, N_TURBINES))
+    yaw_angles[:,:,0] = 5.0
+    floris.farm.yaw_angles = yaw_angles
+    
+    floris.steady_state_atmospheric_condition()
+
+    n_turbines = floris.farm.n_turbines
+    n_wind_speeds = floris.flow_field.n_wind_speeds
+    n_wind_directions = floris.flow_field.n_wind_directions
+
+    velocities = floris.flow_field.u
+    yaw_angles = floris.farm.yaw_angles
+    test_results = np.zeros((n_wind_directions, n_wind_speeds, n_turbines, 4))
+
+    farm_avg_velocities = average_velocity(
+        velocities,
+    )
+    farm_cts = Ct(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    farm_powers = power(
+        floris.flow_field.air_density,
+        velocities,
+        yaw_angles,
+        floris.farm.pP,
+        floris.farm.power_interp,
+    )
+    farm_axial_inductions = axial_induction(
+        velocities,
+        yaw_angles,
+        floris.farm.fCt_interp,
+    )
+    for i in range(n_wind_directions):
+        for j in range(n_wind_speeds):
+            for k in range(n_turbines):
+                test_results[i, j, k, 0] = farm_avg_velocities[i, j, k]
+                test_results[i, j, k, 1] = farm_cts[i, j, k]
+                test_results[i, j, k, 2] = farm_powers[i, j, k]
+                test_results[i, j, k, 3] = farm_axial_inductions[i, j, k]
+
+    if DEBUG:
+        print_test_values(
+            farm_avg_velocities,
+            farm_cts,
+            farm_powers,
+            farm_axial_inductions,
+        )
+
+    assert_results_arrays(test_results[0], secondary_steering_baseline)
