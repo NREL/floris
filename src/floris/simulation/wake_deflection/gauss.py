GCH: bug fix in wake rotation ground effect vortex

src/floris/simulation/wake_deflection/gauss.py
@@ -459,9 +459,9 @@ def calculate_transverse_velocity(
     # wake rotation vortex - ground effect
     zCb = z + HH + NUM_EPS
     rCb = yLocs ** 2 + zCb ** 2
+    core_shape = 1 - np.exp(-rCb / (eps ** 2))
     V6 = (-1 * Gamma_wake_rotation * zCb) / (2 * np.pi * rCb) * core_shape * decay
     W6 = (Gamma_wake_rotation * yLocs) / (2 * np.pi * rCb) * core_shape * decay
-    core_shape = 1 - np.exp(-rCb / (eps ** 2))
 
     # total spanwise velocity
     V = V1 + V2 + V3 + V4 + V5 + V6