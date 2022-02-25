d6afcc3a797c541d56e88af357615de8573a86c4
bayc <christopher.j.bay@gmail.com>
2022-02-24 17:48:17 -0700

    updating the layout optimization example

--- examples/10_optimize_layout.py
+++ examples/10_optimize_layout.py
@@ -14,50 +14,50 @@
 
 
 import os
-
 import numpy as np
-import matplotlib.pyplot as plt
-
-# import floris.tools as wfct
-
 
 from floris.tools import FlorisInterface
 import floris.tools.optimization.pyoptsparse as opt
 
+"""
+This example shows a simple layout optimization using the python module pyOptSparse.
+
+A 4 turbine array is optimized such that the layout of the turbine produces the
+highest annual energy production (AEP) based on the given wind resource. The turbines
+are constrained to a square boundary and a randomw wind resource is supplied. The results
+of the optimization show that the turbines are pushed to the outer corners of the boundary,
+which makes sense in order to maximize the energy production by minimizing wake interactions.
+"""
 
 # Initialize the FLORIS interface fi
 file_dir = os.path.dirname(os.path.abspath(__file__))
 fi = FlorisInterface('inputs/gch.yaml')
+
+# Setup 72 wind directions with a random wind speed and frequency distribution
 wind_directions = np.arange(0, 360.0, 5.0)
-wind_speeds = np.array([8.0])
+np.random.seed(1)
+wind_speeds = 8.0 + np.random.randn(1) * 0.5
+freq = np.abs(np.sort(np.random.randn(len(wind_directions))))
+freq = freq / freq.sum()
 fi.reinitialize(wind_directions=wind_directions, wind_speeds=wind_speeds)
 
+# The boundaries for the turbines, specified as vertices
 boundaries = [(0.0, 0.0), (0.0, 1000.0), (1000.0, 1000.0), (1000.0, 0.0), (0.0, 0.0)]
 
 # Set turbine locations to 4 turbines in a rectangle
-# D = fi.floris.farm.turbines[0].rotor_diameter
-D = 126.0
+D = 126.0 # rotor diameter for the NREL 5MW
 layout_x = [0, 0, 6 * D, 6 * D]
-layout_y = [0, 5 * D, 0, 5 * D]
+layout_y = [0, 4 * D, 0, 4 * D]
 fi.reinitialize(layout=(layout_x, layout_y))
 fi.calculate_wake()
 
-# wd = np.arange(0., 360., 60.)
-# wd = [0, 90, 180, 270]
-wd = [270]
-np.random.seed(1)
-ws = 8.0 + np.random.randn(len(wd)) * 0.5
-freq = np.abs(np.sort(np.random.randn(len(wd))))
-freq = freq / freq.sum()
+# Setup the optimization problem
+model = opt.layout.Layout(fi, boundaries, freq)
+tmp = opt.optimization.Optimization(model=model, solver='SLSQP')
 
-model = opt.layout.Layout(fi, boundaries)
-
-tmp = opt.optimization.Optimization(model=model, solver='SNOPT')
-
+# Run the optimization
 sol = tmp.optimize()
 
+# Print and plot the results
 print(sol)
-
 model.plot_layout_opt_results(sol)
-plt.savefig('fig.png')
-# plt.show()
