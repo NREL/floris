e4d2b7de1185be76b299620ce2841f269678a344
Rafael M Mudafort <rafmudaf@gmail.com>
2022-03-04 16:47:07 -0600

    Store version in a Python file
    This simplifies all of the installation infrastructure. There were a lot of hoops to jump through to include a non-python file in a distribution. We get the same functionality and simplify our lives this way.

--- floris/__init__.py
+++ floris/__init__.py
@@ -14,5 +14,5 @@
 
 from pathlib import Path
 
-with open(Path(__file__).parent / "VERSION") as _version_file:
+with open(Path(__file__).parent / "version.py") as _version_file:
     __version__ = _version_file.read().strip()
