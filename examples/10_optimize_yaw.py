Fix for repeated calls to calculate_wake returning incorrect values. (#381)
* adding _sorted versions of variables (grid, farm, and flow field)

* updating tests to use _sorted variables

Co-authored-by: Rafael M Mudafort <rafmudaf@gmail.com>

examples/10_optimize_yaw.py
@@ -42,8 +42,8 @@ def load_floris():
     # Specify wind farm layout and update in the floris object
     N = 5  # number of turbines per row and per column
     X, Y = np.meshgrid(
-        5.0 * fi.floris.farm.rotor_diameters[0][0][0] * np.arange(0, N, 1),
-        5.0 * fi.floris.farm.rotor_diameters[0][0][0] * np.arange(0, N, 1),
+        5.0 * fi.floris.farm.rotor_diameters_sorted[0][0][0] * np.arange(0, N, 1),
+        5.0 * fi.floris.farm.rotor_diameters_sorted[0][0][0] * np.arange(0, N, 1),
     )
     fi.reinitialize(layout=(X.flatten(), Y.flatten()))
 