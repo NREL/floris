7dfb1c48d97a048880f921a124debf69f3ce2c5e
Rafael M Mudafort <rafmudaf@gmail.com>
2022-04-01 15:48:09 -0500

    Add None-model regression tests

--- tests/reg_tests/none_regression_test.py
+++ tests/reg_tests/none_regression_test.py
@@ -13,6 +13,7 @@
 # See https://floris.readthedocs.io for documentation
 
 import numpy as np
+import pytest
 
 from tests.conftest import N_TURBINES, N_WIND_DIRECTIONS, N_WIND_SPEEDS
 from tests.conftest import print_test_values, assert_results_arrays
@@ -20,8 +21,8 @@ from floris.simulation import Ct, Floris, power, axial_induction, average_veloci
 
 
 DEBUG = False
-VELOCITY_MODEL = "jensen"
-DEFLECTION_MODEL = "jimenez"
+VELOCITY_MODEL = "none"
+DEFLECTION_MODEL = "none"
 
 
 baseline = np.array(
@@ -29,26 +30,26 @@ baseline = np.array(
         # 8 m/s
         [
             [7.9803783, 0.7634300, 1695368.6455473, 0.2568077],
-            [6.1587079, 0.8281078, 771711.1397217, 0.2927005],
-            [5.6650109, 0.8525651, 591201.9530589, 0.3080137],
+            [7.9803783, 0.7634300, 1695368.6455473, 0.2568077],
+            [7.9803783, 0.7634300, 1695368.6455473, 0.2568077],
         ],
         # 9 m/s
         [
             [8.9779256, 0.7625731, 2413659.0651694, 0.2563676],
-            [6.9320582, 0.7949917, 1111095.6755725, 0.2736108],
-            [6.5097475, 0.8119845, 914532.9508510, 0.2831962],
+            [8.9779256, 0.7625731, 2413659.0651694, 0.2563676],
+            [8.9779256, 0.7625731, 2413659.0651694, 0.2563676],
         ],
         # 10 m/s
         [
             [9.9754729, 0.7527803, 3306006.9741814, 0.2513940],
-            [7.7463875, 0.7694786, 1555147.9058151, 0.2599368],
-            [7.3516513, 0.7807166, 1328938.9807448, 0.2658614],
+            [9.9754729, 0.7527803, 3306006.9741814, 0.2513940],
+            [9.9754729, 0.7527803, 3306006.9741814, 0.2513940],
         ],
         # 11 m/s
         [
             [10.9730201, 0.7304328, 4373591.7174990, 0.2404007],
-            [ 8.6282507, 0.7618324, 2145636.4122529, 0.2559879],
-            [ 8.1492608, 0.7625162, 1808686.0209504, 0.2563384],
+            [10.9730201, 0.7304328, 4373591.7174990, 0.2404007],
+            [10.9730201, 0.7304328, 4373591.7174990, 0.2404007],
         ]
     ]
 )
@@ -58,26 +59,26 @@ yawed_baseline = np.array(
         # 8 m/s
         [
             [7.9803783, 0.7605249, 1683956.3885389, 0.2548147],
-            [6.1728455, 0.8274561, 777439.4138493, 0.2923080],
-            [5.6710199, 0.8522576, 593286.4075389, 0.3078136],
+            [7.9803783, 0.7605249, 1683956.3885389, 0.2548147],
+            [7.9803783, 0.7605249, 1683956.3885389, 0.2548147],
         ],
         # 9 m/s
         [
             [8.9779256, 0.7596713, 2397237.3791443, 0.2543815],
-            [6.9479076, 0.7943540, 1118472.7182916, 0.2732589],
-            [6.5163795, 0.8117177, 917619.8050142, 0.2830424],
+            [8.9779256, 0.7596713, 2397237.3791443, 0.2543815],
+            [8.9779256, 0.7596713, 2397237.3791443, 0.2543815],
         ],
         # 10 m/s
         [
             [9.9754729, 0.7499157, 3283592.6005045, 0.2494847],
-            [7.7633174, 0.7690410, 1565293.2751215, 0.2597090],
-            [7.3579657, 0.7805093, 1332282.8722076, 0.2657508],
+            [9.9754729, 0.7499157, 3283592.6005045, 0.2494847],
+            [9.9754729, 0.7499157, 3283592.6005045, 0.2494847],
         ],
         # 11 m/s
         [
             [10.9730201, 0.7276532, 4344217.6993801, 0.2386508],
-            [ 8.6453185, 0.7618685, 2158718.7235002, 0.2560064],
-            [ 8.1535724, 0.7625044, 1811619.6777321, 0.2563324],
+            [10.9730201, 0.7276532, 4344217.6993801, 0.2386508],
+            [10.9730201, 0.7276532, 4344217.6993801, 0.2386508],
         ]
     ]
 )
@@ -240,56 +241,8 @@ def test_regression_yaw(sample_inputs_fixture):
     floris.farm.yaw_angles = yaw_angles
 
     floris.initialize_domain()
-    floris.steady_state_atmospheric_condition()
-
-    n_turbines = floris.farm.n_turbines
-    n_wind_speeds = floris.flow_field.n_wind_speeds
-    n_wind_directions = floris.flow_field.n_wind_directions
-
-    velocities = floris.flow_field.u
-    yaw_angles = floris.farm.yaw_angles
-    test_results = np.zeros((n_wind_directions, n_wind_speeds, n_turbines, 4))
-
-    farm_avg_velocities = average_velocity(
-        velocities,
-    )
-    farm_cts = Ct(
-        velocities,
-        yaw_angles,
-        floris.farm.turbine_fCts,
-        floris.farm.turbine_type_map,
-    )
-    farm_powers = power(
-        floris.flow_field.air_density,
-        velocities,
-        yaw_angles,
-        floris.farm.pPs,
-        floris.farm.turbine_power_interps,
-        floris.farm.turbine_type_map,
-    )
-    farm_axial_inductions = axial_induction(
-        velocities,
-        yaw_angles,
-        floris.farm.turbine_fCts,
-        floris.farm.turbine_type_map,
-    )
-    for i in range(n_wind_directions):
-        for j in range(n_wind_speeds):
-            for k in range(n_turbines):
-                test_results[i, j, k, 0] = farm_avg_velocities[i, j, k]
-                test_results[i, j, k, 1] = farm_cts[i, j, k]
-                test_results[i, j, k, 2] = farm_powers[i, j, k]
-                test_results[i, j, k, 3] = farm_axial_inductions[i, j, k]
-
-    if DEBUG:
-        print_test_values(
-            farm_avg_velocities,
-            farm_cts,
-            farm_powers,
-            farm_axial_inductions,
-        )
-
-    assert_results_arrays(test_results[0], yawed_baseline)
+    with pytest.raises(ValueError):
+        floris.steady_state_atmospheric_condition()
 
 
 def test_regression_small_grid_rotation(sample_inputs_fixture):
