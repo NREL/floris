ca6362f7836653da709367aa7855544736853a0a
Rafael M Mudafort <rafmudaf@gmail.com>
2022-04-06 15:45:52 -0500

    Create all Grid data at initialization (#410)
    * Move grid.finalize into initialization
    
    * Update getting-started Notebook

--- floris/simulation/floris.py
+++ floris/simulation/floris.py
@@ -194,8 +194,6 @@ class Floris(logging_manager.LoggerBase, FromDictMixin):
     def finalize(self):
         # Once the wake calculation is finished, unsort the values to match
         # the user-supplied order of things.
-
-        self.grid.finalize()
         self.flow_field.finalize(self.grid.unsorted_indices)
         self.farm.finalize(self.grid.unsorted_indices)
 
