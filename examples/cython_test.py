060d4618a51e72bf6da54f56acee81d97d4cc5c5
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2022-07-26 12:40:20 -0600

    add working example to test speedups as size grows in three dimensions

---
+++ examples/cython_test.py
@@ -0,0 +1,63 @@
+import time
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from floris.tools import FlorisInterface
+
+
+# Initialize the FLORIS interface fi
+file_dir = Path(__file__).resolve().parent
+fi = FlorisInterface(file_dir / "inputs" / "gch.yaml")
+
+D = 126.0  # rotor diameter for the NREL 5MW
+
+
+def change_parameter_space(n_dir, n_speed, grid_dim):
+    # Create the parameter space
+    wind_directions = np.linspace(0, 360, n_dir)
+    wind_speeds = np.linspace(0, 40, n_speed)
+    layout_x = np.array([list(range(grid_dim)) for _ in range(grid_dim)]).flatten() * 6 * D
+    layout_y = np.array([list(range(grid_dim)) for _ in range(grid_dim)]).T.flatten() * 4 * D
+
+    # Reinitialize and calculate wake
+    start = time.perf_counter()
+    fi.reinitialize(
+        layout_x=layout_x,
+        layout_y=layout_y,
+        wind_directions=wind_directions,
+        wind_speeds=wind_speeds,
+    )
+    end = time.perf_counter()
+    fi.calculate_wake()
+    end_2 = time.perf_counter()
+
+    # return reinitialize timing, wake timing, and total timing
+    return end - start, end_2 - end, end_2 - start
+
+
+n_dir = range(5, 361, 5)
+n_speed = range(5, 41, 5)
+grid_dim = range(1, 20)
+
+speed = []
+for i in n_dir:
+    for j in n_speed:
+        for k in grid_dim:
+            init, wake, total = change_parameter_space(i, j, k)
+            speed.append([i, j, k * k, init, wake, total])
+            time.sleep(5)
+
+df = pd.DataFrame(
+    speed,
+    columns=[
+        "n_wind_directions",
+        "n_wind_speeds",
+        "n_turbines",
+        "reinitialize_time",
+        "calculate_wake_time",
+        "total_run_time",
+    ],
+)
+df.to_csv("python_timing.csv")
