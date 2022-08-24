3b1ec38da0a12c6e99faeb7582043b7c12c64431
bayc <christopher.j.bay@gmail.com>
2022-08-24 14:29:10 -0600

    update copyright year for reg_tests

--- tests/reg_tests/gauss_regression_test.py
+++ tests/reg_tests/gauss_regression_test.py
@@ -1,4 +1,4 @@
-# Copyright 2021 NREL
+# Copyright 2022 NREL
 
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
