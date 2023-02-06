2794c6b15e0bba507e21b85717b582fab02a8f6b
Paul <paul.fleming@nrel.gov>
2023-02-06 16:20:48 -0700

    Comment out forced error

--- examples/16b_heterogenaity_multiple_ws_wd.py
+++ examples/16b_heterogenaity_multiple_ws_wd.py
@@ -83,11 +83,12 @@ print('With wind directions now set to 270 and 275 deg')
 print(f'T0: {turbine_powers[:, :, 0].flatten()} kW')
 print(f'T1: {turbine_powers[:, :, 1].flatten()} kW')
 
-# Note if we change wind directions to 3 without a matching change to het map we get an error
-print()
-print()
-print('~~ Now forcing an error by not matching wd and het_map')
+# Uncomment if want to see example of error output
+# # Note if we change wind directions to 3 without a matching change to het map we get an error
+# print()
+# print()
+# print('~~ Now forcing an error by not matching wd and het_map')
 
-fi.reinitialize(wind_directions=[270, 275, 280], wind_speeds=[8.])
-fi.calculate_wake()
-turbine_powers = np.round(fi.get_turbine_powers() / 1000.)
+# fi.reinitialize(wind_directions=[270, 275, 280], wind_speeds=[8.])
+# fi.calculate_wake()
+# turbine_powers = np.round(fi.get_turbine_powers() / 1000.)
