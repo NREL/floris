a68d2cdbc88c313a4f9ed8b2349c15ac15883450
Rob Hammond <13874373+RHammond2@users.noreply.github.com>
2023-02-07 09:05:41 -0700

    enable future typing patterns

--- floris/simulation/solver.py
+++ floris/simulation/solver.py
@@ -10,6 +10,8 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+from __future__ import annotations
+
 import copy
 from abc import abstractmethod
 
