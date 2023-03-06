d24adf504d951931c6e1238e937e586e0681fbd4
Rafael M Mudafort <rafmudaf@gmail.com>
2023-03-06 13:14:15 -0600

    Remove option to use layout tuple

--- floris/tools/floris_interface.py
+++ floris/tools/floris_interface.py
@@ -214,7 +214,6 @@ class FlorisInterface(LoggerBase):
         # with_resolution: float | None = None,
         solver_settings: dict | None = None,
         time_series: bool = False,
-        layout: tuple[list[float], list[float]] | tuple[NDArrayFloat, NDArrayFloat] | None = None,
         het_map=None,
     ):
         # Export the floris object recursively as a dictionary
@@ -243,13 +242,6 @@ class FlorisInterface(LoggerBase):
             self.het_map = het_map
 
         ## Farm
-        if layout is not None:
-            self.logger.warning(
-                "Use the `layout_x` and `layout_y` parameters in place of `layout` "
-                "because the `layout` parameter will be deprecated in 3.3."
-            )
-            layout_x = layout[0]
-            layout_y = layout[1]
         if layout_x is not None:
             farm_dict["layout_x"] = layout_x
         if layout_y is not None:
