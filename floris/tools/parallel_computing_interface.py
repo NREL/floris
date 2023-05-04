09b4aa1e389d7708345e01366bf9c36922edfdfe
bayc <christopher.j.bay@gmail.com>
2023-05-03 23:30:22 -0600

    updating parallel_computing_interface to use het_config

--- floris/tools/parallel_computing_interface.py
+++ floris/tools/parallel_computing_interface.py
@@ -12,17 +12,17 @@ from floris.tools.uncertainty_interface import FlorisInterface, UncertaintyInter
 
 def _load_local_floris_object(
     fi_dict,
-    het_map=None,
+    het_config=None,
     unc_pmfs=None,
     fix_yaw_in_relative_frame=False
 ):
     # Load local FLORIS object
     if unc_pmfs is None:
-        fi = FlorisInterface(fi_dict, het_map=het_map)
+        fi = FlorisInterface(fi_dict, het_config=het_config)
     else:
         fi = UncertaintyInterface(
             fi_dict,
-            het_map=het_map,
+            het_config=het_config,
             unc_pmfs=unc_pmfs,
             fix_yaw_in_relative_frame=fix_yaw_in_relative_frame,
         )
