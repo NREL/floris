9c5d1994feb4a117ac86ff6f0bc9717e768ab5f5
misi9170 <michael.sinner@nrel.gov>
2023-07-06 16:48:42 -0600

    Moving warning from power() to get_turbine_powers() on FlorisInterface.

--- floris/tools/floris_interface.py
+++ floris/tools/floris_interface.py
@@ -595,6 +595,10 @@ class FlorisInterface(LoggerBase):
                 "Can't run function `FlorisInterface.get_turbine_powers` without "
                 "first running `FlorisInterface.calculate_wake`."
             )
+        # Check for negative velocities, which could indicate bad model
+        # parameters or turbines very closely spaced.
+        if (self.turbine_effective_velocities < 0.).any():
+            print("WARNING: Some rotor effective velocities are negative.")
 
         turbine_powers = power(
             ref_density_cp_ct=self.floris.farm.ref_density_cp_cts,
