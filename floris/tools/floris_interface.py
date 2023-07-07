e6906feebdee6bdd2103f0bd390679e6a1b0052d
Rafael M Mudafort <rafmudaf@gmail.com>
2023-07-07 13:16:12 -0500

    Infrastructure updates (#677)
    * Remove unused code
    
    * Expand description of design considerations
    
    * Configure logging immediately
    
    This allows for using it during init
    
    * Update timing plots

--- floris/tools/floris_interface.py
+++ floris/tools/floris_interface.py
@@ -987,15 +987,3 @@ class FlorisInterface(LoggerBase):
             return xcoords, ycoords, zcoords
         else:
             return xcoords, ycoords
-
-
-## Functionality removed in v3
-
-def set_rotor_diameter(self, rotor_diameter):
-    """
-    This function has been replaced and no longer works correctly, assigning an error
-    """
-    raise Exception(
-        "FlorinInterface.set_rotor_diameter has been removed in favor of "
-        "FlorinInterface.change_turbine. See examples/change_turbine/."
-    )
