f5b699fc165dcedc0f57dae8fcbb4196dc80979f
misi9170 <michael.sinner@nrel.gov>
2023-09-01 13:50:18 -0600

    fi_subset needed updating rather than fi.

--- floris/tools/optimization/layout_optimization/layout_optimization_base.py
+++ floris/tools/optimization/layout_optimization/layout_optimization_base.py
@@ -77,7 +77,7 @@ class LayoutOptimization(LoggerBase):
         # NOTE: requires that child class saves x and y locations
         # as self.x and self.y and updates them during optimization.
         if self.enable_geometric_yaw:
-            self.yaw_opt.fi.reinitialize(layout_x=self.x, layout_y=self.y)
+            self.yaw_opt.fi_subset.reinitialize(layout_x=self.x, layout_y=self.y)
             df_opt = self.yaw_opt.optimize()
             self.yaw_angles = np.vstack(df_opt['yaw_angles_opt'])[:, None, :]
         else:
