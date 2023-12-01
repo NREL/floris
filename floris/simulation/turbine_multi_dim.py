bcfd4678efcb023d1f96283f8c6750911d400242
RHammond2 <13874373+RHammond2@users.noreply.github.com>
2023-12-01 09:11:40 -0800

    update default multi turbine path to turbine library

--- floris/simulation/turbine_multi_dim.py
+++ floris/simulation/turbine_multi_dim.py
@@ -34,6 +34,7 @@ from floris.simulation import (
 )
 from floris.simulation.turbine import _filter_convert
 from floris.type_dec import (
+    convert_to_path,
     NDArrayBool,
     NDArrayFilter,
     NDArrayFloat,
@@ -432,12 +433,13 @@ class TurbineMultiDimensional(Turbine):
         turbine_library_path (:py:obj:`pathlib.Path`, optional): The
             :py:attr:`Farm.turbine_library_path` or :py:attr:`Farm.internal_turbine_library_path`,
             whichever is being used to load turbine definitions.
-            Defaults to the current file location.
+            Defaults to the internal turbine library.
     """
     power_thrust_data_file: str = field(default=None)
     multi_dimensional_cp_ct: bool = field(default=False)
     turbine_library_path: Path = field(
-        default=Path(".").resolve(),
+        default=Path(__file__).parents[1] / "turbine_library",
+        converter=convert_to_path,
         validator=attrs.validators.instance_of(Path)
     )
 
@@ -462,9 +464,7 @@ class TurbineMultiDimensional(Turbine):
 
     def __attrs_post_init__(self) -> None:
         # Solidify the data file path and name
-        self.power_thrust_data_file = (
-            self.turbine_library_path / self.power_thrust_data_file
-        ).resolve()
+        self.power_thrust_data_file = self.turbine_library_path / self.power_thrust_data_file
 
         # Read in the multi-dimensional data supplied by the user.
         df = pd.read_csv(self.power_thrust_data_file)
