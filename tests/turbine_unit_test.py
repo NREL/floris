203cf37050571d9025f99da35e5512dca392926d
misi9170 <michael.sinner@nrel.gov>
2023-12-27 10:06:32 -0700

    Update imports in tests.

--- tests/turbine_unit_test.py
+++ tests/turbine_unit_test.py
@@ -28,7 +28,7 @@ from floris.simulation import (
     power,
     Turbine,
 )
-from floris.simulation.turbine.turbine import (
+from floris.simulation.turbine.rotor_effective_velocity import (
     _rotor_velocity_tilt_correction,
     _rotor_velocity_yaw_correction,
     compute_tilt_angles_for_floating_turbines,
