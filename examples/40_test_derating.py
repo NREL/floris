09ffafcd6ed59debf159abf23b59e74f03144239
misi9170 <michael.sinner@nrel.gov>
2024-02-05 13:23:44 -0700

    Test example a bit more built out.

--- examples/40_test_derating.py
+++ examples/40_test_derating.py
@@ -12,7 +12,7 @@
 
 # See https://floris.readthedocs.io for documentation
 
-
+import matplotlib.pyplot as plt
 import numpy as np
 import yaml
 
@@ -38,11 +38,30 @@ fi.reinitialize(layout_x=[0, 500.0], layout_y=[0.0, 0.0], turbine_type=[turbine_
 
 # Set the wind directions and speeds to be constant over n_findex = N time steps
 N = 50
-fi.reinitialize(wind_directions=270 * np.ones(N), wind_speeds=8.0 * np.ones(N))
+fi.reinitialize(wind_directions=270 * np.ones(N), wind_speeds=10.0 * np.ones(N))
 fi.calculate_wake()
 turbine_powers_orig = fi.get_turbine_powers()
 
 # Add derating
-power_setpoints = np.tile(np.linspace(0, 6e3, N), 2).reshape(N, 2)
+power_setpoints = np.tile(np.linspace(1, 6e6, N), 2).reshape(2, N).T
 fi.calculate_wake(power_setpoints=power_setpoints)
 turbine_powers_derated= fi.get_turbine_powers()
+
+# Plot the results
+fig, ax = plt.subplots(1, 1)
+ax.plot(power_setpoints[:, 0]/1000, turbine_powers_derated[:, 0]/1000, color="C0", label="Upstream")
+ax.plot(power_setpoints[:, 1]/1000, turbine_powers_derated[:, 1]/1000, color="C1",
+    label="Downstream")
+ax.plot(power_setpoints[:, 0]/1000, turbine_powers_orig[:, 0]/1000, color="C0", linestyle="dotted",
+    label="Upstream available")
+ax.plot(power_setpoints[:, 1]/1000, turbine_powers_orig[:, 1]/1000, color="C1", linestyle="dotted",
+    label="Downstream available")
+ax.plot(power_setpoints[:, 1]/1000, np.ones(N)*np.max(turbine_type["power_thrust_table"]["power"]),
+    color="k", linestyle="dashed", label="Rated power")
+ax.grid()
+ax.legend()
+ax.set_xlim([0, 6e3])
+ax.set_xlabel("Power setpoint (kW)")
+ax.set_ylabel("Power produced (kW)")
+
+plt.show()
