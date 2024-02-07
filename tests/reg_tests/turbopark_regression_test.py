abb31577778eedeb5222b9fd96f65e76d83971d4
Paul <paul.fleming@nrel.gov>
2024-02-07 11:19:54 -0700

    Fix mismatch in ws/wd sizes in regression tests

--- tests/reg_tests/turbopark_regression_test.py
+++ tests/reg_tests/turbopark_regression_test.py
@@ -226,7 +226,7 @@ def test_regression_rotation(sample_inputs_fixture):
         5 * TURBINE_DIAMETER
     ]
     sample_inputs_fixture.floris["flow_field"]["wind_directions"] = [270.0, 360.0]
-    sample_inputs_fixture.floris["flow_field"]["wind_speeds"] = [8.0]
+    sample_inputs_fixture.floris["flow_field"]["wind_speeds"] = [8.0, 8.0]
 
     floris = Floris.from_dict(sample_inputs_fixture.floris)
     floris.initialize_domain()
