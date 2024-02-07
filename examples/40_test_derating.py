6aac1efd04bc9e657e20d21840d61acb3a93f361
misi9170 <michael.sinner@nrel.gov>
2024-02-07 12:35:01 -0700

    Example improvements.

--- examples/40_test_derating.py
+++ examples/40_test_derating.py
@@ -35,7 +35,7 @@ with open(str(
 turbine_type["power_thrust_model"] = "simple-derating"
 
 # Convert to a simple two turbine layout with derating turbines
-fi.reinitialize(layout_x=[0, 500.0], layout_y=[0.0, 0.0], turbine_type=[turbine_type])
+fi.reinitialize(layout_x=[0, 1000.0], layout_y=[0.0, 0.0], turbine_type=[turbine_type])
 
 # Set the wind directions and speeds to be constant over n_findex = N time steps
 N = 50
@@ -46,19 +46,42 @@ turbine_powers_orig = fi.get_turbine_powers()
 # Add derating
 power_setpoints = np.tile(np.linspace(1, 6e6, N), 2).reshape(2, N).T
 fi.calculate_wake(power_setpoints=power_setpoints)
-turbine_powers_derated= fi.get_turbine_powers()
+turbine_powers_derated = fi.get_turbine_powers()
+
+# Compute available power at downstream turbine
+power_setpoints_2 = np.array([np.linspace(1, 6e6, N), np.full(N, None)]).T
+fi.calculate_wake(power_setpoints=power_setpoints_2)
+turbine_powers_avail_ds = fi.get_turbine_powers()[:,1]
 
 # Plot the results
 fig, ax = plt.subplots(1, 1)
 ax.plot(power_setpoints[:, 0]/1000, turbine_powers_derated[:, 0]/1000, color="C0", label="Upstream")
-ax.plot(power_setpoints[:, 1]/1000, turbine_powers_derated[:, 1]/1000, color="C1",
-    label="Downstream")
-ax.plot(power_setpoints[:, 0]/1000, turbine_powers_orig[:, 0]/1000, color="C0", linestyle="dotted",
-    label="Upstream available")
-ax.plot(power_setpoints[:, 1]/1000, turbine_powers_orig[:, 1]/1000, color="C1", linestyle="dotted",
-    label="Downstream available")
-ax.plot(power_setpoints[:, 1]/1000, np.ones(N)*np.max(turbine_type["power_thrust_table"]["power"]),
-    color="k", linestyle="dashed", label="Rated power")
+ax.plot(
+    power_setpoints[:, 1]/1000,
+    turbine_powers_derated[:, 1]/1000,
+    color="C1",
+    label="Downstream"
+)
+ax.plot(
+    power_setpoints[:, 0]/1000,
+    turbine_powers_orig[:, 0]/1000,
+    color="C0",
+    linestyle="dotted",
+    label="Upstream available"
+)
+ax.plot(
+    power_setpoints[:, 1]/1000,
+    turbine_powers_avail_ds/1000,
+    color="C1",
+    linestyle="dotted", label="Downstream available"
+)
+ax.plot(
+    power_setpoints[:, 1]/1000,
+    np.ones(N)*np.max(turbine_type["power_thrust_table"]["power"]),
+    color="k",
+    linestyle="dashed",
+    label="Rated power"
+)
 ax.grid()
 ax.legend()
 ax.set_xlim([0, 6e3])
