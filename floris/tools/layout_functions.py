a720f9cf04cd9809186fd5e5b22286444f3293af
Paul <paul.fleming@nrel.gov>
2024-02-15 13:44:59 -0700

    Add get_wake_direction

--- floris/tools/layout_functions.py
+++ floris/tools/layout_functions.py
@@ -23,3 +23,28 @@ import numpy as np
 import pandas as pd
 from scipy.spatial.distance import pdist, squareform
 
+
+def get_wake_direction(x_i, y_i, x_j, y_j):
+    """
+    Calculates the compass direction where turbine i wakes turbine j
+
+    Args:
+        x_i: x-coordinate of the starting point
+        y_i: y-coordinate of the starting point
+        x_j: x-coordinate of the ending point
+        y_j: y-coordinate of the ending point
+
+    Returns:
+        wake_direction (float): Angle in degrees, when turbine i wakes turbine j
+    """
+
+    dx = x_j - x_i
+    dy = y_j - y_i
+
+    angle_rad = np.arctan2(dy, dx)
+    angle_deg = 270 - np.rad2deg(angle_rad)
+
+    # Adjust for "from" direction (add 180 degrees) and wrap within 0-360
+    wind_direction = angle_deg % 360
+
+    return wind_direction
