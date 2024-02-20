b158f635c060e0cc76687632939d2ac16ac02148
Paul <paul.fleming@nrel.gov>
2024-02-20 14:33:11 -0700

    Add tests of plots

--- tests/layout_functions_test.py
+++ tests/layout_functions_test.py
@@ -1,7 +1,15 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
 import floris.tools.layout_functions as lf
+from floris.tools.floris_interface import FlorisInterface
+
+
+TEST_DATA = Path(__file__).resolve().parent / "data"
+YAML_INPUT = TEST_DATA / "input_full.yaml"
 
 
 def test_get_wake_direction():
@@ -13,3 +21,13 @@ def test_get_wake_direction():
 
     # Winds from the south
     assert np.isclose(lf.get_wake_direction(0, -1, 0, 0), 180.0)
+
+def test_plotting_functions():
+
+    fi = FlorisInterface(configuration=YAML_INPUT)
+    ax = lf.plot_turbine_points(fi=fi)
+    assert isinstance(ax, plt.Axes)
+    ax = lf.plot_turbine_labels(fi=fi)
+    assert isinstance(ax, plt.Axes)
+    ax = lf.plot_waking_directions(fi=fi)
+    assert isinstance(ax, plt.Axes)
