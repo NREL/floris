37f336e2352a6e39171805bd3ad0883fa49cc452
paulf81 <paul.fleming@nrel.gov>
2024-02-20 18:19:20 -0700

    Change from Apache to BSD 3-clause license (#810)
    * Update license file to bsd
    
    * Update setup file to bsd
    
    * Update README.md
    
    * Remove header block everywhere
    
    * Fix end of file
    
    * Use a consistent format for beginning files
    
    https://github.com/NREL/floris/pull/810#issuecomment-1955186596
    
    * Wrap lines in license statements
    
    ---------
    
    Co-authored-by: Rafael M Mudafort <rafmudaf@gmail.com>

--- examples/41_test_disable_turbines.py
+++ examples/41_test_disable_turbines.py
@@ -1,19 +1,3 @@
-# Copyright 2023 NREL
-
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy of
-# the License at http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
-
-# See https://floris.readthedocs.io for documentation
-
-# Example adapted from https://github.com/NREL/floris/pull/693 contributed by Elie Kadoche
-
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,6 +7,7 @@ from floris.tools import FlorisInterface
 
 
 """
+Adapted from https://github.com/NREL/floris/pull/693 contributed by Elie Kadoche
 This example demonstrates the ability of FLORIS to shut down some turbines
 during a simulation.
 """
