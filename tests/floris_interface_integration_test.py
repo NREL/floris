e083ced1eb98d227c7de31849537f0afead3f74f
Paul <paul.fleming@nrel.gov>
2024-02-26 09:06:09 -0700

    Fix the bug in test_disable_turbines

--- tests/floris_interface_integration_test.py
+++ tests/floris_interface_integration_test.py
@@ -284,10 +284,14 @@ def test_disable_turbines():
     fi.run()
     power_with_middle_disabled = fi.get_turbine_powers()
 
-    disable_turbines = np.zeros((2, 2), dtype=bool)
-    fi.set(layout_x=[0,2000], layout_y=[0, 0], disable_turbines=disable_turbines)
-    fi.run()
-    power_with_middle_removed = fi.get_turbine_powers()
+    # Set up case with middle turbine removed
+    disable_turbines_remove_middle = np.zeros((2, 2), dtype=bool)
+    fi_remove_middle = fi.copy()
+    fi_remove_middle.set(layout_x=[0,2000],
+                          layout_y=[0, 0],
+                            disable_turbines=disable_turbines_remove_middle)
+    fi_remove_middle.run()
+    power_with_middle_removed = fi_remove_middle.get_turbine_powers()
 
     np.testing.assert_almost_equal(power_with_middle_disabled[0,2], power_with_middle_removed[0,1])
     np.testing.assert_almost_equal(power_with_middle_disabled[1,2], power_with_middle_removed[1,1])
