df1db4b85d530cee65cde5358afb93a6a3cc09f1
Paul <paul.fleming@nrel.gov>
2024-03-06 22:18:41 -0700

    Update example 10

--- examples/10_opt_yaw_single_ws.py
+++ examples/10_opt_yaw_single_ws.py
@@ -22,12 +22,14 @@ fi = FlorisInterface("inputs/gch.yaml") # GCH model matched to the default "lega
 # Reinitialize as a 3-turbine farm with range of WDs and 1 WS
 wd_array = np.arange(0.0, 360.0, 3.0)
 ws_array = 8.0 * np.ones_like(wd_array)
+turbulence_intensities = 0.06 * np.ones_like(wd_array)
 D = 126.0 # Rotor diameter for the NREL 5 MW
 fi.set(
     layout_x=[0.0, 5 * D, 10 * D],
     layout_y=[0.0, 0.0, 0.0],
     wind_directions=wd_array,
     wind_speeds=ws_array,
+    turbulence_intensities=turbulence_intensities,
 )
 print(fi.floris.farm.rotor_diameters)
 
