4ed058bd925acc4ad8b7a1abd788a85945900cc6
misi9170 <michael.sinner@nrel.gov>
2024-03-12 12:06:48 -0600

    Added tests for unc+parallel model; currently failing.

--- tests/parallel_floris_model_integration_test.py
+++ tests/parallel_floris_model_integration_test.py
@@ -3,7 +3,7 @@ import copy
 
 import numpy as np
 
-from floris import FlorisModel, ParallelFlorisModel
+from floris import FlorisModel, ParallelFlorisModel, UncertainFlorisModel
 from tests.conftest import (
     assert_results_arrays,
 )
@@ -30,6 +30,39 @@ def test_parallel_turbine_powers(sample_inputs_fixture):
     fmodel.run()
 
     serial_turbine_powers = fmodel.get_turbine_powers()
+
+    pfmodel = ParallelFlorisModel(
+        fmodel=pfmodel_input,
+        max_workers=2,
+        n_wind_condition_splits=2,
+        interface="concurrent",
+        print_timings=False,
+    )
+
+    parallel_turbine_powers = pfmodel.get_turbine_powers()
+
+    if DEBUG:
+        print(serial_turbine_powers)
+        print(parallel_turbine_powers)
+
+    assert_results_arrays(parallel_turbine_powers, serial_turbine_powers)
+
+def test_parallel_uncertain_turbine_powers(sample_inputs_fixture):
+    """
+
+    """
+    sample_inputs_fixture.core["wake"]["model_strings"]["velocity_model"] = VELOCITY_MODEL
+    sample_inputs_fixture.core["wake"]["model_strings"]["deflection_model"] = DEFLECTION_MODEL
+
+    ufmodel = UncertainFlorisModel(
+        sample_inputs_fixture.core,
+        wd_sample_points=[-3, 0, 3],
+        wd_std=3
+    )
+    pfmodel_input = copy.deepcopy(ufmodel)
+    ufmodel.run()
+
+    serial_turbine_powers = ufmodel.get_turbine_powers()
 
     pfmodel = ParallelFlorisModel(
         fmodel=pfmodel_input,
