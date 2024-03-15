7ded17b9d25b0f93741818c150eb4bace82937dd
Paul <paul.fleming@nrel.gov>
2024-03-15 12:13:10 -0600

    formatting

--- examples/004_set.py
+++ examples/004_set.py
@@ -75,12 +75,12 @@ fmodel.set(layout_x=[0, 500.0], layout_y=[0.0, 0.0])
 ######################################################
 
 # Changes to controls settings can be made using the set method
-# Note the dimension must match (n_findex, n_tindex) or (number of conditions, number of turbines)
-# Above we n_findex = 3 and n_tindex = 2 so the matrix of yaw angles must be 3x2
+# Note the dimension must match (n_findex, n_turbines) or (number of conditions, number of turbines)
+# Above we n_findex = 3 and n_turbines = 2 so the matrix of yaw angles must be 3x2
 yaw_angles = np.array([[0.0, 0.0], [25.0, 0.0], [0.0, 0.0]])
 fmodel.set(yaw_angles=yaw_angles)
 
-# By default for the turbines in the turbine_libary, the power
+# By default for the turbines in the turbine_library, the power
 # thrust model is set to "cosine-loss" which adjusts
 # power and thrust according to cos^cosine_loss_exponent(yaw | tilt)
 # where the default exponent is 1.88.  For other
@@ -105,7 +105,7 @@ disable_turbines = np.array([[True, False], [True, False], [False, False]])
 fmodel.set(disable_turbines=disable_turbines)
 
 # Derate the front turbine for the first two findex
-RATED_POWER = 5e6  # 5MW
+RATED_POWER = 5e6  # 5MW (Anything above true rated power will still result in rated power)
 power_setpoints = np.array(
     [[RATED_POWER * 0.3, RATED_POWER], [RATED_POWER * 0.3, RATED_POWER], [RATED_POWER, RATED_POWER]]
 )
