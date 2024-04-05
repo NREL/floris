99161f24d887c1e5d0c4f26dba0666f9d7007ed0
misi9170 <39596329+misi9170@users.noreply.github.com>
2024-04-04 20:36:52 -0400

    [BUGFIX] set_operation_model ordering with layout (#867)
    * Handling for order of set_operation model and setting layout.
    
    * ruff.
    
    * One more test added.

--- floris/core/farm.py
+++ floris/core/farm.py
@@ -209,7 +209,8 @@ class Farm(BaseClass):
         if len(value) != 1 and len(value) != self.n_turbines:
             raise ValueError(
                 "turbine_type must have the same number of entries as layout_x/layout_y or have "
-                "a single turbine_type value."
+                "a single turbine_type value. This error can arise if you set the turbine_type or "
+                "alter the operation model before setting the layout."
             )
 
     @turbine_library_path.validator
