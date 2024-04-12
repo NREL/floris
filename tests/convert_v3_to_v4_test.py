5e9938f939a2c613c92f2b004893493543e4c19d
Paul <paul.fleming@nrel.gov>
2024-04-12 15:45:56 -0600

    Add a test of v3->v4 conversion

--- tests/convert_v3_to_v4_test.py
+++ tests/convert_v3_to_v4_test.py
@@ -1,2 +1,49 @@
+import os
+from pathlib import Path
+
+from floris import FlorisModel
+
+
+CONVERT_FOLDER = Path(__file__).resolve().parent / "v3_to_v4_convert_test"
+FLORIS_FOLDER = Path(__file__).resolve().parent / ".." / "floris"
+
+
 def test_v3_to_v4_convert():
-    pass
+    # Note certain filenames
+    filename_v3_floris = "gch.yaml"
+    filename_v4_floris = "gch_v4.yaml"
+    filename_v3_turbine = "nrel_5MW_v3.yaml"
+    filename_v4_turbine = "nrel_5MW_v3_v4.yaml"
+
+    # Copy convert scripts from FLORIS_FOLDER to CONVERT_FOLDER
+    os.system(f"cp {FLORIS_FOLDER / 'convert_turbine_v3_to_v4.py'} {CONVERT_FOLDER}")
+    os.system(f"cp {FLORIS_FOLDER / 'convert_floris_input_v3_to_v4.py'} {CONVERT_FOLDER}")
+
+    # Change directory to the test folder
+    os.chdir(CONVERT_FOLDER)
+
+    # Print the current directory
+    print(os.getcwd())
+
+    # Run the converter on the turbine file
+    os.system(f"python convert_turbine_v3_to_v4.py {filename_v3_turbine}")
+
+    # Run the converter on the floris file
+    os.system(f"python convert_floris_input_v3_to_v4.py {filename_v3_floris}")
+
+    # Go through the file filename_v4_turbine and where the place-holder string "XXXXX" is found
+    # replace it with the string f"!include {filename_v4_turbine}"
+    with open(filename_v4_floris, "r") as file:
+        filedata = file.read()
+    filedata = filedata.replace("XXXXX", f"!include {filename_v4_turbine}")
+    with open(filename_v4_floris, "w") as file:
+        file.write(filedata)
+
+    # Now confirm that the converted file can be loaded by FLORIS
+    _ = FlorisModel(filename_v4_floris)
+
+    # Delete the newly created files to clean up
+    os.system(f"rm {filename_v4_floris}")
+    os.system(f"rm {filename_v4_turbine}")
+    os.system("rm convert_turbine_v3_to_v4.py")
+    os.system("rm convert_floris_input_v3_to_v4.py")
