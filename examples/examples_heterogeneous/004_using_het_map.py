527283f0276572e92556a229b15dc5fbbd249d72
Paul <paul.fleming@nrel.gov>
2024-05-08 22:26:29 -0600

    Update example

--- examples/examples_heterogeneous/004_using_het_map.py
+++ examples/examples_heterogeneous/004_using_het_map.py
@@ -10,14 +10,14 @@ from floris.heterogeneous_map import HeterogeneousMap
 
 
 heterogeneous_inflow_config_by_wd = {
-    "x": np.array([0.0, 0.0, 500.0, 500.0]),
-    "y": np.array([0.0, 500.0, 0.0, 500.0 ]),
+    "x": np.array([0.0, 0.0, 250.0,500.0, 500.0]),
+    "y": np.array([0.0, 500.0, 250.0,0.0, 500.0 ]),
     "speed_multipliers": np.array(
         [
-            [1.0, 1.0, 1.0, 1.0],
-            [1.0, 1.25, 1.0, 1.25],
-            [1.0, 1.0, 1.0, 1.25],
-            [1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.25, 1.0, 1.0],
+            [1.0, 1.0, 1.25, 1.0, 1.0],
         ]
     ),
     "wind_directions": np.array([0.0, 0.0, 90.0, 90.0]),
@@ -25,3 +25,7 @@ heterogeneous_inflow_config_by_wd = {
 }
 
 hm = HeterogeneousMap(**heterogeneous_inflow_config_by_wd)
+
+hm.plot_single_speed_multiplier(wind_direction=85.0, wind_speed=8.0)
+
+plt.show()
