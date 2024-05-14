e9531b69e1faef2cd4713ee0e15783c2845fc8b6
JasperShell <jasper.kreeft@shell.com>
2024-05-14 09:45:49 +0200

    Test case files added

---
+++ PR_TurbOParkGauss/Case_Rowpark_TurbOPark.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from floris import FlorisModel
+
+fmodel1 = FlorisModel("Case_RowPark_TurbOPark.yaml")
+fmodel1.run()
+raws1 = fmodel1.turbine_average_velocities
+
+u0 = fmodel1.wind_speeds
+
+fmodel2 = FlorisModel("Case_RowPark_TurbOParkGauss.yaml")
+fmodel2.run()
+raws2 = fmodel2.turbine_average_velocities
+
+df = pd.read_csv("/mnt/c/Users/Jasper.Kreeft/Data/PYTHON/NREL/floris4/PR_TurbOParkGauss/Rowpark_Orsted.csv")
+
+fig, ax = plt.subplots()
+ax.scatter(range(1,11),raws1/u0,s=80,marker='p',label='Floris - TurbOPark')
+ax.scatter(range(1,11),raws2/u0,s=80,marker='^',label='Floris - TurbOPark_Gauss')
+ax.plot(df.values[:,0],df.values[:,1],'or',label='Orsted - TurbOPark')
+ax.set_xlabel('Turbine number [-]')
+ax.set_ylabel('Normalized waked wind speed')
+ax.set_xlim(0,11)
+ax.set_ylim(0.25,1.05)
+ax.legend()
+
+plt.show()
---
+++ PR_TurbOParkGauss/Case_TwinPark_TurbOPark_implementation.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from floris import (
+    FlorisModel,
+    TimeSeries,
+)
+
+fmodel = FlorisModel("Case_TwinPark_TurbOPark.yaml")
+
+wd_array = np.arange(225,315,0.1)
+fmodel.set(
+    wind_data=TimeSeries(
+        wind_speeds=8.0, wind_directions=wd_array, turbulence_intensities=0.06
+    )
+)
+
+fmodel.run()
+
+rawss = fmodel.turbine_average_velocities
+raws = rawss[:,1]
+u0 = fmodel.wind_speeds
+
+###
+
+fmodel2 = FlorisModel("Case_TwinPark_TurbOParkGauss.yaml")
+
+wd_array = np.arange(225,315,0.1)
+fmodel2.set(
+    wind_data=TimeSeries(
+        wind_speeds=8.0, wind_directions=wd_array, turbulence_intensities=0.06
+    )
+)
+
+fmodel2.run()
+
+rawss2 = fmodel2.turbine_average_velocities
+raws2 = rawss2[:,1]
+
+###
+df = pd.read_csv("/mnt/c/Users/Jasper.Kreeft/Data/PYTHON/NREL/floris4/PR_TurbOParkGauss/WindDirection_Sweep_Orsted.csv")
+
+###
+fig, ax1 = plt.subplots()
+ax1.plot(wd_array,raws/u0,label='Floris 4 - TurbOPark')
+ax1.plot(wd_array,raws2/u0,label='Floris 4 - TurbOPark-Gauss')
+ax1.plot(df.values[:,0],df.values[:,1],'--',label='Orsted - TurbOPark')
+
+ax1.set_xlabel('Wind Direction (deg)')
+ax1.set_ylabel('Normalized rotor averaged waked wind speed [-]')
+ax1.set_xlim(240,300)
+ax1.set_ylim(0.65,1.05)
+ax1.legend()
+plt.show()
