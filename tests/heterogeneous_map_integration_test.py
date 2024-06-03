ab4b4f4c5d92d3d92cbfefb67591f5032a2f7005
Paul <paul.fleming@nrel.gov>
2024-06-03 06:40:45 -0600

    Add a test the 3d het and shear yield matching values

--- tests/heterogeneous_map_integration_test.py
+++ tests/heterogeneous_map_integration_test.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 import numpy as np
 import pytest
 
+from floris import FlorisModel
 from floris.heterogeneous_map import HeterogeneousMap
+
+
+TEST_DATA = Path(__file__).resolve().parent / "data"
+YAML_INPUT = TEST_DATA / "input_full.yaml"
 
 
 def test_declare_by_parameters():
@@ -290,3 +297,100 @@ def test_get_2d_heterogenous_map_from_3d():
     assert hm_3d.wind_speeds is None
     assert hm_2d_0.wind_speeds is None
     assert hm_2d_1.wind_speeds is None
+
+
+def test_3d_het_and_shear():
+    # Define a 3D het map with 4 z locations and a single x and y where
+    # the speed ups are defined by the usual power low
+    wind_speed = 8.0
+    z_values_array = np.array([0.0, 100.0, 200.0, 300.0])
+    reference_wind_height = 90.0
+    wind_shear = 0.12
+    speed_multipliers_array = (z_values_array / reference_wind_height) ** wind_shear
+
+    # Define the x and y locations to be corners of a square that repeats
+    # for 4 different z locations
+    x_values = np.tile(np.array([-500.0, 500.0, -500.0, 500.0]), 4)
+    y_values = np.tile(np.array([-500.0, -500.0, 500.0, 500.0]), 4)
+
+    # Repeat each of the elements of z_values 4 times
+    z_values = np.repeat(z_values_array, 4)
+    speed_multipliers = np.repeat(speed_multipliers_array, 4)
+
+    # Reshape speed_multipliers to be (1,16)
+    speed_multipliers = speed_multipliers.reshape(1, -1)
+
+    # Build the 3d HeterogeneousMap
+    hm_3d = HeterogeneousMap(
+        x=x_values,
+        y=y_values,
+        z=z_values,
+        speed_multipliers=speed_multipliers,
+    )
+
+    # Get the FLORIS model
+    fmodel = FlorisModel(configuration=YAML_INPUT)
+
+    # Set the model to a single wind direction, speed and turbine
+    fmodel.set(
+        wind_directions=[270.0],
+        wind_speeds=[wind_speed],
+        layout_x=[0],
+        layout_y=[0],
+        wind_shear=wind_shear,
+    )
+
+    # Run the model
+    fmodel.run()
+
+    # Get the velocities 100 m in front of the turbine
+    # Use the calculate_y_plane method because sample_flow_at_points does not currently work
+    # with heterogeneous inflow
+    # u_at_points_shear = fmodel.sample_flow_at_points(x= -100.0 * np.ones_like(z_values_array),
+    #                                               y= 0.0 * np.ones_like(z_values_array),
+    #                                               z=z_values_array)
+    y_plane = fmodel.calculate_y_plane(
+        x_resolution=1,
+        z_resolution=4,
+        crossstream_dist=0.0,
+        x_bounds=[-100.0, -100.0],
+        z_bounds=[0.0, 300.0],
+    )
+    u_at_points_shear = y_plane.df["u"].values
+
+    # Check that the velocities are as expected, ie the shear exponent of 0.12
+    # produces speeds expeted from the power law
+    assert np.allclose(u_at_points_shear, wind_speed * speed_multipliers_array)
+
+    # Now change the model such that shear is 0, while the 3D het map is applied
+    fmodel.set(
+        wind_shear=0.0,
+        heterogeneous_inflow_config=hm_3d.get_heterogeneous_inflow_config(
+            wind_directions=[270.0], wind_speeds=[wind_speed]
+        ),
+    )
+
+    # Run the model
+    fmodel.run()
+
+    # # Get the velocities 100 m in front of the turbine
+    y_plane_het = fmodel.calculate_y_plane(
+        x_resolution=1,
+        z_resolution=4,
+        crossstream_dist=0.0,
+        x_bounds=[-100.0, -100.0],
+        z_bounds=[0.0, 300.0],
+    )
+    u_at_points_het = y_plane_het.df["u"].values
+
+    # Confirm this produces the same results as the shear model
+    assert np.allclose(u_at_points_het, u_at_points_shear)
+
+    # Finally confirm that applying both shear and het raises a value error
+    with pytest.raises(ValueError):
+        fmodel.set(
+            wind_shear=0.12,
+            heterogeneous_inflow_config=hm_3d.get_heterogeneous_inflow_config(
+                wind_directions=[270.0], wind_speeds=[wind_speed]
+            ),
+        )
