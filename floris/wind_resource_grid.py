7368ea6b300840fad714e4ff180ef346bff8b456
Paul <paul.fleming@nrel.gov>
2024-06-07 17:25:51 -0600

    Update wind_resource_grid and tests

--- floris/wind_resource_grid.py
+++ floris/wind_resource_grid.py
@@ -1,5 +1,7 @@
 import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 
+from floris import WindRose
 from floris.logging_manager import LoggingManager
 
 
@@ -21,6 +23,14 @@ class WindResourceGrid(LoggingManager):
         self.read_wrg_file(filename)
 
     def read_wrg_file(self, filename):
+        """
+        Read the contents of a WRG file and store the data in the object.
+
+        Args:
+            filename (str): The name of the WRG file to read.
+
+        """
+
         # Read the file into data
         with open(filename, "r") as f:
             data = f.readlines()
@@ -48,35 +58,231 @@ class WindResourceGrid(LoggingManager):
 
         # Initialize the data arrays which have the same number of
         # elements as the number of grid points
-        self.x = np.zeros(self.n_gid)
-        self.y = np.zeros(self.n_gid)
-        self.z = np.zeros(self.n_gid)
-        self.h = np.zeros(self.n_gid)
+        x_gid = np.zeros(self.n_gid)
+        y_gid = np.zeros(self.n_gid)
+        z_gid = np.zeros(self.n_gid)
+        h_gid = np.zeros(self.n_gid)
 
         # Initialize the data arrays which are n_gid x n_sectors
-        self.sector_freq = np.zeros((self.n_gid, self.n_sectors))
-        self.weibull_A = np.zeros((self.n_gid, self.n_sectors))
-        self.weibull_k = np.zeros((self.n_gid, self.n_sectors))
+        sector_freq_gid = np.zeros((self.n_gid, self.n_sectors))
+        weibull_A_gid = np.zeros((self.n_gid, self.n_sectors))
+        weibull_k_gid = np.zeros((self.n_gid, self.n_sectors))
 
         # Loop through the data and extract the values
         for gid in range(self.n_gid):
             line = data[1 + gid]
-            self.x[gid] = float(line[10:20])
-            self.y[gid] = float(line[20:30])
-            self.z[gid] = float(line[30:38])
-            self.h[gid] = float(line[38:43])
+            x_gid[gid] = float(line[10:20])
+            y_gid[gid] = float(line[20:30])
+            z_gid[gid] = float(line[30:38])
+            h_gid[gid] = float(line[38:43])
 
             for sector in range(self.n_sectors):
                 # The frequency of the wind in this sector is in probablility * 1000
-                self.sector_freq[gid, sector] = (
+                sector_freq_gid[gid, sector] = (
                     float(line[72 + sector * 13 : 76 + sector * 13]) / 1000.0
                 )
 
                 # The A and k parameters are in the next 10 characters, with A stored * 10
                 # and k stored * 100
-                self.weibull_A[gid, sector] = (
-                    float(line[76 + sector * 13 : 80 + sector * 13]) / 10.0
-                )
-                self.weibull_k[gid, sector] = (
+                weibull_A_gid[gid, sector] = float(line[76 + sector * 13 : 80 + sector * 13]) / 10.0
+                weibull_k_gid[gid, sector] = (
                     float(line[80 + sector * 13 : 85 + sector * 13]) / 100.0
                 )
+
+        # Save a single value of z and h for the entire grid
+        self.z = z_gid[0]
+        self.h = h_gid[0]
+
+        # Index the by sector data by x and y
+        self.sector_freq = np.zeros((self.nx, self.ny, self.n_sectors))
+        self.weibull_A = np.zeros((self.nx, self.ny, self.n_sectors))
+        self.weibull_k = np.zeros((self.nx, self.ny, self.n_sectors))
+
+        for x_idx, x in enumerate(self.x_array):
+            for y_idx, y in enumerate(self.y_array):
+                # Find the indices when x_gid and y_gid are equal to x and y
+                idx = np.where((x_gid == x) & (y_gid == y))[0]
+
+                # Assign the data to the correct location
+                self.sector_freq[x_idx, y_idx, :] = sector_freq_gid[idx, :]
+                self.weibull_A[x_idx, y_idx, :] = weibull_A_gid[idx, :]
+                self.weibull_k[x_idx, y_idx, :] = weibull_k_gid[idx, :]
+
+        # Build the interpolant function lists
+        self.interpolant_sector_freq = self._build_interpolant_function_list(
+            self.x_array, self.y_array, self.n_sectors, self.sector_freq
+        )
+        self.interpolant_weibull_A = self._build_interpolant_function_list(
+            self.x_array, self.y_array, self.n_sectors, self.weibull_A
+        )
+        self.interpolant_weibull_k = self._build_interpolant_function_list(
+            self.x_array, self.y_array, self.n_sectors, self.weibull_k
+        )
+
+    def _build_interpolant_function_list(self, x, y, n_sectors, data):
+        """
+        Build a list of interpolant functions for the data.  It is assumed that the function
+        should return a list of interpolant functions, length n_sectors.
+
+        Args:
+            x (np.array): The x values of the data, length nx.
+            y (np.array): The y values of the data, length ny.
+            n_sectors (int): The number of sectors.
+            data (np.array): The data to interpolate, shape (nx, ny, n_sectors).
+
+        Returns:
+            list: A list of interpolant functions, length n_sectors.
+        """
+
+        function_list = []
+
+        for sector in range(n_sectors):
+            function_list.append(
+                RegularGridInterpolator(
+                    (x, y),
+                    data[:, :, sector],
+                    bounds_error=False,
+                    fill_value=None,
+                )
+            )
+
+        return function_list
+
+    def _interpolate_data(self, x, y, interpolant_function_list):
+        """
+        Interpolate the data at a given x, y location using the interpolant function list.
+
+        Args:
+            x (float): The x location to interpolate.
+            y (float): The y location to interpolate.
+            interpolant_function_list (list): A list of interpolant functions.
+
+        Returns:
+            list: A list of interpolated data, length n_sectors.
+        """
+
+        # Check if x and y are within the bounds of the self.x_array and self.y_array, if
+        # so use the nearest method, otherwise use the linear method of interpolation
+        if (
+            x < self.x_array[0]
+            or x > self.x_array[-1]
+            or y < self.y_array[0]
+            or y > self.y_array[-1]
+        ):
+            method = "nearest"
+        else:
+            method = "linear"
+
+        result = np.zeros(self.n_sectors)
+        for sector in range(self.n_sectors):
+            result[sector] = interpolant_function_list[sector]((x, y), method=method)
+
+        return result
+
+    def _weibull_cumulative(self, x, a, k):
+        """
+        Calculate the Weibull cumulative distribution function.
+        """
+
+        exponent = -((x / a) ** k)
+        result = 1.0 - np.exp(exponent)
+
+        # Where x is less than 0, the result should be 0
+        result[x < 0] = 0.0
+
+        return result
+
+        # Original code from PJ Stanley
+        # if x >= 0.0:
+        #     exponent = -(x / a) ** k
+        #     return 1.0 - np.exp(exponent)
+        # else:
+        #     return 0.0
+
+    def _generate_wind_speed_frequencies_from_weibull(self, A, k, wind_speeds=None):
+        """
+        Generate the wind speed frequencies from the Weibull parameters.  Use the
+        cumulative form of the function and calculate the probability of the wind speed
+        in a given bin via the difference in the cumulative function at the bin edges.
+        Args:
+
+            A (np.array): The Weibull A parameter.
+            k (np.array): The Weibull k parameter.
+            wind_speeds (np.array): The wind speeds to calculate the frequencies for.
+                If None, the frequencies are calculated for 0 to 25 m/s in 1 m/s increments.
+                Default is None.
+
+        Returns:
+            np.array: The wind speed frequencies.
+        """
+
+        if wind_speeds is None:
+            wind_speeds = np.arange(0.0, 25.0, 1.0)
+
+        # Define the wind speed edges
+        ws_step = wind_speeds[1] - wind_speeds[0]
+        wind_speed_edges = np.arange(
+            wind_speeds[0] - ws_step / 2, wind_speeds[-1] + ws_step, ws_step
+        )
+
+        # Get the cumulative distribution function at the edges
+        cdf_edges = self._weibull_cumulative(wind_speed_edges, A, k)
+
+        # The frequency is the difference in the cumulative distribution function
+        # at the edges
+        freq = cdf_edges[1:] - cdf_edges[:-1]
+
+        # Normalize the frequency
+        # TODO: This is perhaps not quite right, if the user only asks
+        # for wind speeds at say 8 and 10 m/s
+        # then the bins would be from 7 to 11 and so fairly there is
+        # distribution outside the range, but
+        # I think this is on the whole right -- PF
+        freq = freq / freq.sum()
+
+        return wind_speeds, freq
+
+    def get_wind_rose_at_point(self, x, y, wind_speeds=None, fixed_ti_value=0.06):
+        """
+        Get the wind rose at a given x, y location.  Interpolate the parameters to the point
+        and then generate the wind rose.
+
+        Args:
+            x (float): The x location to interpolate.
+            y (float): The y location to interpolate.
+            wind_speeds (np.array): The wind speeds to calculate the frequencies for.
+                If None, the frequencies are calculated for 0 to 25 m/s in 1 m/s increments.
+                Default is None.
+            fixed_ti_value (float): The fixed turbulence intensity value to use in the wind rose.
+                Default is 0.06.
+        """
+
+        if wind_speeds is None:
+            wind_speeds = np.arange(0.0, 25.0, 1.0)
+
+        # Get the interpolated data
+        sector_freq = self._interpolate_data(x, y, self.interpolant_sector_freq)
+        weibull_A = self._interpolate_data(x, y, self.interpolant_weibull_A)
+        weibull_k = self._interpolate_data(x, y, self.interpolant_weibull_k)
+
+        # Initialize the freq_table
+        freq_table = np.zeros((self.n_sectors, len(wind_speeds)))
+
+        # First fill in the rows of the table using the weibull distributions,
+        # weighted by the sector freq
+        for sector in range(self.n_sectors):
+            wind_speeds, freq = self._generate_wind_speed_frequencies_from_weibull(
+                weibull_A[sector], weibull_k[sector], wind_speeds=wind_speeds
+            )
+            freq_table[sector, :] = sector_freq[sector] * freq
+
+        # Normalize the table
+        freq_table = freq_table / freq_table.sum()
+
+        # Return the wind rose
+        return WindRose(
+            wind_directions=self.wind_directions,
+            wind_speeds=wind_speeds,
+            freq_table=freq_table,
+            ti_table=fixed_ti_value,
+        )
--- tests/wind_resource_grid_integration_test.py
+++ tests/wind_resource_grid_integration_test.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import numpy as np
 import pytest
+from scipy.optimize import curve_fit
 
 from floris.wind_resource_grid import WindResourceGrid
 
@@ -26,18 +27,18 @@ def test_read_header():
 
     wrg = WindResourceGrid(WRG_FILE_FILE)
 
-    assert wrg.nx == 3
+    assert wrg.nx == 2
     assert wrg.ny == 3
     assert wrg.xmin == 0.0
     assert wrg.ymin == 0.0
     assert wrg.grid_size == 1000.0
 
     # Test x and y arrays
-    assert np.allclose(wrg.x_array, np.array([0.0, 1000.0, 2000.0]))
+    assert np.allclose(wrg.x_array, np.array([0.0, 1000.0]))
     assert np.allclose(wrg.y_array, np.array([0.0, 1000.0, 2000.0]))
 
     # Test number of grid points
-    assert wrg.n_gid == 9
+    assert wrg.n_gid == 6
 
     # Test number of sectors
     assert wrg.n_sectors == 12
@@ -53,22 +54,123 @@ def test_read_data():
 
     wrg = WindResourceGrid(WRG_FILE_FILE)
 
-    # Test x, y, z, and h arrays
-    assert np.allclose(
-        wrg.x, np.array([0.0, 0.0, 0.0, 1000.0, 1000.0, 1000.0, 2000.0, 2000.0, 2000.0])
-    )
-    assert np.allclose(
-        wrg.y, np.array([0.0, 1000.0, 2000.0, 0.0, 1000.0, 2000.0, 0.0, 1000.0, 2000.0])
-    )
-    assert np.allclose(wrg.z, np.zeros(9))
-    assert np.allclose(wrg.h, 90.0 * np.ones(9))
+    # Test z and h values
+    assert wrg.z == 0.0
+    assert wrg.h == 90.0
 
     # Test the first and last gid for sector_freq, weibull_A, and weibull_k
-    assert wrg.sector_freq[0, 0] == 116 / 1000.0
-    assert wrg.sector_freq[-1, -1] == 31 / 1000.0
+    assert wrg.sector_freq[0, 0, 0] == 116 / 1000.0
+    assert wrg.sector_freq[-1, -1, -1] == 34 / 1000.0
 
-    assert wrg.weibull_A[0, 0] == 106 / 10.0
-    assert wrg.weibull_A[-1, -1] == 90 / 10.0
+    assert wrg.weibull_A[0, 0, 0] == 106 / 10.0
+    assert wrg.weibull_A[-1, -1, -1] == 81 / 10.0
 
-    assert wrg.weibull_k[0, 0] == 273 / 100.0
-    assert wrg.weibull_k[-1, -1] == 231 / 100.0
+    assert wrg.weibull_k[0, 0, 0] == 273 / 100.0
+    assert wrg.weibull_k[-1, -1, -1] == 199 / 100.0
+
+
+def test_build_interpolant_function_list():
+    wrg = WindResourceGrid(WRG_FILE_FILE)
+
+    # Initialize the values
+    x = np.array([0.0, 1000.0])
+    y = np.array([0.0, 500.0, 1000.0])
+    n_sectors = 3
+    data = np.ones((2, 3, 3))
+
+    # For the first sector, the point at (1000, 0) is 2.0
+    data[1, 0, 0] = 2.0
+
+    # For the second sector, the point at (1000, 500) is 3.0
+    data[1, 1, 1] = 3.0
+
+    function_list = wrg._build_interpolant_function_list(x, y, n_sectors, data)
+
+    # Length of the function list should be n_sectors
+    assert len(function_list) == n_sectors
+
+    # Test the interpolation in the first sector
+    test_value = function_list[0]((500, 0))
+    assert test_value == 1.5
+
+    # Test the interpolation in the second sector
+    test_value = function_list[1]((1000, 250))
+    assert test_value == 2.0
+
+    # Test using linear method
+    test_value = function_list[0]((500, 0), method="linear")
+    assert test_value == 1.5
+
+    # Test using nearest method
+    test_value = function_list[0]((1001, 0), method="nearest")
+    assert test_value == 2.0
+
+
+def test_interpolate_data():
+    wrg = WindResourceGrid(WRG_FILE_FILE)
+
+    sector_freq = wrg.sector_freq
+
+    # Test that interpolating onto the point 0,0 returns the 1st row of the sector_freq
+    test_value = wrg._interpolate_data(0, 0, wrg.interpolant_sector_freq)
+    assert np.allclose(test_value, sector_freq[0, 0, :])
+
+    # Test the interpolating just out of bounds of 0,0 also yields the 1st row of the sector_freq
+    test_value = wrg._interpolate_data(-1, -1, wrg.interpolant_sector_freq)
+    assert np.allclose(test_value, sector_freq[0, 0, :])
+
+    # Test that value at x=500, y0, this is the average of the rows at [0,0] and [1,0]
+    test_value = wrg._interpolate_data(500, 0, wrg.interpolant_sector_freq)
+    assert np.allclose(test_value, (sector_freq[0, 0, :] + sector_freq[1, 0, :]) / 2)
+
+    # Test that value at x=0, y=500, this is the average of the rows at [0,0] and [0,1]
+    test_value = wrg._interpolate_data(0, 500, wrg.interpolant_sector_freq)
+    assert np.allclose(test_value, (sector_freq[0, 0, :] + sector_freq[0, 1, :]) / 2)
+
+
+def test_generate_wind_speed_frequencies_from_weibull():
+    wrg = WindResourceGrid(WRG_FILE_FILE)
+
+    wind_speeds_in = np.array([0.0, 5.0, 10.0, 15.0])
+    wind_speeds, freq = wrg._generate_wind_speed_frequencies_from_weibull(
+        10.0, 2.0, wind_speeds=wind_speeds_in
+    )
+
+    # Test that the wind speeds are the same
+    assert np.allclose(wind_speeds, wind_speeds_in)
+
+    # Test that freq is the same length as wind_speeds
+    assert len(freq) == len(wind_speeds)
+
+    # Test that the frequencies sum to 1.0
+    assert np.allclose(np.sum(freq), 1.0)
+
+    # Test the correctness of the frequencies by reversing the process
+    wind_speeds = np.arange(0.0, 100.0, 1.0)
+    A_test = 9.0
+    k_test = 1.8
+    wind_speeds, freq = wrg._generate_wind_speed_frequencies_from_weibull(
+        A_test, k_test, wind_speeds=wind_speeds
+    )
+
+    # Test that the mean value matches theory
+    mean_value = np.sum(wind_speeds * freq)
+    assert np.allclose(mean_value, A_test * np.math.gamma(1 + 1 / k_test), rtol=0.1)
+
+
+def test_get_wind_rose_at_point():
+    wrg = WindResourceGrid(WRG_FILE_FILE)
+
+    wind_speeds = np.arange(0.0, 25.0, 1.0)
+    n_wind_speeds = len(wind_speeds)
+
+    wind_rose = wrg.get_wind_rose_at_point(0, 0)
+
+    # Test that there are 12 wind directions at n_wind_speeds wind speeds
+    assert wind_rose.freq_table.shape == (12, n_wind_speeds)
+    assert len(wind_rose.wind_speeds) == n_wind_speeds
+    assert len(wind_rose.wind_directions) == 12
+
+    # Test that freq table generated at (0, 0) is the same at that of (-1 , -1)
+    wind_rose2 = wrg.get_wind_rose_at_point(-1, -1)
+    assert np.allclose(wind_rose.freq_table, wind_rose2.freq_table)
