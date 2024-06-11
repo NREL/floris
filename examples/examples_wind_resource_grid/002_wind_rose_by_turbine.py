8df01fa0e100563ac72538b3503627d007e1c131
Paul <paul.fleming@nrel.gov>
2024-06-10 22:22:52 -0600

    Update example

--- examples/examples_wind_resource_grid/002_wind_rose_by_turbine.py
+++ examples/examples_wind_resource_grid/002_wind_rose_by_turbine.py
@@ -10,7 +10,11 @@ turbine.
 import matplotlib.pyplot as plt
 import numpy as np
 
-from floris import WindResourceGrid, WindRoseByTurbine
+from floris import (
+    FlorisModel,
+    WindResourceGrid,
+    WindRoseByTurbine,
+)
 
 
 # Read the WRG file
@@ -24,10 +28,34 @@ layout_x = np.array([0, 500, 1000])
 layout_y = np.array([0, 1000, 2000])
 
 # Get the WindRoseByTurbine object
+wind_rose_by_turbine = wrg.get_wind_rose_by_turbine(layout_x, layout_y)
 
-wrt = wrg.get_wind_rose_by_turbine(layout_x, layout_y)
-
-# Plot the wind roses
+# Plot the wind roses in the first figure
 fig, axarr = plt.subplots(1, 3, figsize=(16, 5), subplot_kw={"polar": True})
-wrt.plot_wind_roses(axarr=axarr, ws_step=5)
-plt.show()
+wind_rose_by_turbine.plot_wind_roses(axarr=axarr, ws_step=5)
+
+# Now apply the wind_rose_by_turbine to a FlorisModel
+fmodel = FlorisModel("../inputs/gch.yaml")
+fmodel.set(layout_x=layout_x, layout_y=layout_y, wind_data=wind_rose_by_turbine)
+fmodel.run()
+expected_turbine_powers = fmodel.get_expected_turbine_powers()
+
+# Compare with the result if just using the first wind rose or the last wind rose
+fmodel.set(wind_data=wind_rose_by_turbine.wind_roses[0])
+fmodel.run()
+expected_turbine_powers_first = fmodel.get_expected_turbine_powers()
+
+fmodel.set(wind_data=wind_rose_by_turbine.wind_roses[-1])
+fmodel.run()
+expected_turbine_powers_last = fmodel.get_expected_turbine_powers()
+
+# Print the results
+print("Expected turbine powers:")
+print("All wind roses:", expected_turbine_powers)
+print("First wind rose:", expected_turbine_powers_first)
+print("Last wind rose:", expected_turbine_powers_last)
+
+# Compare expected farm power against the sum of expected_turbine_power
+expected_farm_power = fmodel.get_expected_farm_power()
+print("Expected farm power:", expected_farm_power)
+print("Sum of expected turbine powers:", expected_turbine_powers.sum())
