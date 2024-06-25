3c49940fbecf9e74c42070426cd0540ea63b9df2
Paul <paul.fleming@nrel.gov>
2024-06-25 16:54:18 -0600

    Update example 001

--- examples/examples_wind_resource_grid/001_wind_resource_grid.py
+++ examples/examples_wind_resource_grid/001_wind_resource_grid.py
@@ -1,30 +1,28 @@
 """Example: Wind Resource Grid
 
-Open the WRG file generated in the previous example using the `WindResourceGrid` object
+Open the WRG file generated in the previous example using the `WindRoseWRG` object
 and show that wind roses can be interpolated between grid points.
 
 """
 import matplotlib.pyplot as plt
 import numpy as np
 
-from floris import WindResourceGrid
+from floris import WindRoseWRG
 
 
 # Read the WRG file
-wrg = WindResourceGrid("wrg_example.wrg")
+wind_rose_wrg = WindRoseWRG("wrg_example.wrg")
 
 # Print some basic information
-print(wrg)
+print(wind_rose_wrg)
 
-# The wind roses were set to rotate at each movement along the y-direction,
-# show that the points in between the grid points have wind roses derived
-# by interpolating the provided sector probabilities, and Weibull parameters.
+# The wind roses were set to ...
 y_points_to_test = np.array([0, 500, 1000, 1500, 2000])
 
 fig, axarr = plt.subplots(1, 5, figsize=(16, 5), subplot_kw={"polar": True})
 
 for i in range(5):
-    wind_rose = wrg.get_wind_rose_at_point(0, y_points_to_test[i])
+    wind_rose = wind_rose_wrg.get_wind_rose_at_point(0, y_points_to_test[i])
     wind_rose.plot(ax=axarr[i], ws_step=5)
     if i %2 == 0:
         axarr[i].set_title(f"y = {y_points_to_test[i]}")
