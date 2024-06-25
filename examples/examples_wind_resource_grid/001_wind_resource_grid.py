69dfdd42070a2a6b27cd70f501c46a30f6ef260a
Paul <paul.fleming@nrel.gov>
2024-06-25 17:05:40 -0600

    Update example 1

--- examples/examples_wind_resource_grid/001_wind_resource_grid.py
+++ examples/examples_wind_resource_grid/001_wind_resource_grid.py
@@ -1,4 +1,4 @@
-"""Example: Wind Resource Grid
+"""Example: WindRoseWRG
 
 Open the WRG file generated in the previous example using the `WindRoseWRG` object
 and show that wind roses can be interpolated between grid points.
@@ -16,7 +16,9 @@ wind_rose_wrg = WindRoseWRG("wrg_example.wrg")
 # Print some basic information
 print(wind_rose_wrg)
 
-# The wind roses were set to ...
+# The wind roses were set to have a higher concenctration of faster north winds for
+# increasing y, show that this is contained within the wind roses, even those interpolated
+# between grid points
 y_points_to_test = np.array([0, 500, 1000, 1500, 2000])
 
 fig, axarr = plt.subplots(1, 5, figsize=(16, 5), subplot_kw={"polar": True})
@@ -28,5 +30,17 @@ for i in range(5):
         axarr[i].set_title(f"y = {y_points_to_test[i]}")
     else:
         axarr[i].set_title(f"y = {y_points_to_test[i]}\n(Interpolated)")
+
+# Go through the axarr and delete the legends
+for ax in axarr:
+    ax.legend().set_visible(False)
+
+
+# Draw a horizontal line on each axis indicating the level of the lower wind speed
+# bucket for the north wind from the first wind rose
+for i in range(5):
+    axarr[i].axhline(y=0.036, color="red", alpha=0.5)
+
+
 
 plt.show()
