e5658fc9d8aba2523333c98978c8ac9026b1f1c9
Paul <paul.fleming@nrel.gov>
2024-06-26 22:27:08 -0600

    Update example 1

--- examples/examples_wind_resource_grid/001_wind_rose_wrg.py
+++ examples/examples_wind_resource_grid/001_wind_rose_wrg.py
@@ -9,7 +9,7 @@ layout.
 In this example the WRG file generated in the previous example is read in
 using the `WindRoseWRG` object, and wind roses as points on the WRG grid, as will
 as in-between interpolated points have wind roses calculated using the `get_wind_rose_at_point`
-method.
+method.  Finally, the wind roses are upsampled to 5 degree wind direction bins and plotted.
 
 """
 import matplotlib.pyplot as plt
@@ -52,5 +52,26 @@ for i in range(5):
 fig.suptitle("Wind Roses at locations with increasing y.  Note the location where the 5 m/s bin \
 transitions to 10 m/s for north wind at y = 0 is \nindicated by the red line to show \
 the increase in wind speed to the north as y increases.")
+
+# Since wind directions was not specified, the wind directions implied by the number of sectors
+# in the WRG was used, however the wind directions can be set using the set_wind_directions method
+# or passed in at initialization.  Here we upsample from 12, 30-deg sectors, to 72 5-deg sectors
+wind_rose_wrg.set_wd_step(5.0)
+
+fig, axarr = plt.subplots(1, 5, figsize=(16, 5), subplot_kw={"polar": True})
+
+for i in range(5):
+    wind_rose = wind_rose_wrg.get_wind_rose_at_point(0, y_points_to_test[i])
+    wind_rose.plot(ax=axarr[i], ws_step=5)
+    if i %2 == 0:
+        axarr[i].set_title(f"y = {y_points_to_test[i]}")
+    else:
+        axarr[i].set_title(f"y = {y_points_to_test[i]}\n(Interpolated)")
+
+# Go through the axarr and delete all the legends except for the middle
+for ax in axarr:
+    ax.legend().set_visible(False)
+
+fig.suptitle('Wind roses with upsampling to 5-deg bins')
 
 plt.show()
