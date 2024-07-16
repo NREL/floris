44e0e039ac0f2d0f02641a21593f0654d10a28af
misi9170 <michael.sinner@nrel.gov>
2024-07-15 19:17:07 -0600

    Update 002 example.

--- examples/examples_ev/002_reproduce_published_results.py
+++ examples/examples_ev/002_reproduce_published_results.py
@@ -9,74 +9,80 @@ Links:
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
-from floris import FlorisModel, TimeSeries
+from floris import FlorisModel
 from floris.turbine_library import build_cosine_loss_turbine_dict
 
 
 # Build a constant CT turbine model for use in comparisons (not realistic)
-D = 120.0 # rotor diameter [m]
-HH = 100.0 # hub height [m]
 u_0 = 8.0 # wind speed [m/s]
 
 # Load the EV model
 fmodel = FlorisModel("../inputs/ev.yaml")
 
-## First, attempt to reproduce Figs. 3 and 4 from Ainslie (1988)
+## First, attempt to reproduce Figs. 3 and 4 from Ainslie (1988).
 
-# TODO: set up model parameters to match Ainslie (if possible)
-# z_h = 50
-# z_o = 0.05
-# K_M / (U_H D) = 0.023s
-fmodel.set_param(["wake", "wake_velocity_parameters", "eddy_viscosity", "filter_cutoff_D"], 0.0)
+# It is not clear exactly how the parametrization of Gunn, which is the one
+# that is implemented in the EddyViscosityVelocity model, can be matched to
+# the parameters given by Ainslie. Rather than try to do this, we simply
+# generate plots similar to Figs. 3 and 4. using the default parameters following
+# Gunn (2019).
 
 # Generate figure to plot on
-fig, ax = plt.subplots(1, 1)
-fig.set_size_inches(4, 4)
+fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)
+fig.set_size_inches(8, 4)
 
-for C_T, ls in zip([0.5, 0.7, 0.9], ["-", "--", ":"]):
-    const_CT_turb = build_cosine_loss_turbine_dict(
-        turbine_data_dict={
-            "wind_speed":[0.0, 30.0],
-            "power":[0.0, 1.0], # Not realistic but won't be used here
-            "thrust_coefficient":[C_T, C_T]
-        },
-        turbine_name="ConstantCT",
-        rotor_diameter=D,
-        hub_height=HH,
-        ref_tilt=0.0,
-    )
+HH = 50
+D = 50
 
-    # Load the EV model and set a constant CT turbine
-    fmodel.set(
-        layout_x=[0],
-        layout_y=[0],
-        turbine_type=[const_CT_turb],
-        wind_speeds=[u_0],
-        wind_directions=[270],
-        turbulence_intensities=[0.14],
-        wind_shear=0.0
-    )
+for i, wd_std_ev in enumerate([0.0, np.rad2deg(0.1)]):
+    fmodel.set_param(["wake", "wake_velocity_parameters", "eddy_viscosity", "wd_std_ev"], wd_std_ev)
+    for C_T, ls in zip([0.5, 0.7, 0.9], ["-", "--", ":"]):
+        const_CT_turb = build_cosine_loss_turbine_dict(
+            turbine_data_dict={
+                "wind_speed":[0.0, 30.0],
+                "power":[0.0, 1.0], # Not realistic but won't be used here
+                "thrust_coefficient":[C_T, C_T]
+            },
+            turbine_name="ConstantCT",
+            rotor_diameter=D,
+            hub_height=HH,
+            ref_tilt=0.0,
+        )
 
-    points_x = np.linspace(2*D, 10*D, 1000)
-    points_y = np.zeros_like(points_x)
-    points_z = HH * np.ones_like(points_x)
+        # Load the EV model and set a constant CT turbine
+        fmodel.set(
+            layout_x=[0],
+            layout_y=[0],
+            turbine_type=[const_CT_turb],
+            wind_speeds=[u_0],
+            wind_directions=[270],
+            turbulence_intensities=[0.14], # As specified by Ainslie
+            wind_shear=0.0
+        )
 
-    u_at_points = fmodel.sample_flow_at_points(points_x, points_y, points_z)
+        points_x = np.linspace(2*D, 10*D, 1000)
+        points_y = np.zeros_like(points_x)
+        points_z = HH * np.ones_like(points_x)
 
-    # Plot results (not different axis scales in Ainslie)
-    ax.plot(
-        points_x/D, 1-u_at_points[0, :]/u_0, color="black", linestyle=ls, label=rf"$C_T$ = {C_T}"
-    )
+        u_at_points = fmodel.sample_flow_at_points(points_x, points_y, points_z)
 
-ax.set_xlabel("Downstream distance [D]")
-ax.set_ylabel("Centerline velocity deficit [-]")
-ax.set_ylim([0, 1])
-ax.legend()
-ax.grid()
+        # Plot results (not different axis scales in Ainslie)
+        ax[i].plot(
+            points_x/D, 1-u_at_points[0, :]/u_0, color="k", linestyle=ls, label=rf"$C_T$ = {C_T}"
+        )
+        ax[i].set_title(r"WD std. dev. $\sigma_{\theta} = $"+"{:.1f} deg".format(wd_std_ev))
 
-## Second, reproduce Figure 1 from Gunn (2019)
+ax[0].set_xlabel("Downstream distance [D]")
+ax[1].set_xlabel("Downstream distance [D]")
+ax[0].set_ylabel("Centerline velocity deficit [-]")
+ax[0].set_ylim([0, 1])
+ax[0].legend()
+ax[0].grid()
+ax[1].grid()
+
+
+## Second, reproduce Figure 1b (right) from Gunn (2019)
 
 # Reset to the default model parameters
 fmodel = FlorisModel("../inputs/ev.yaml")
@@ -88,8 +94,9 @@ D2 = 1.52
 y_offset = -20
 HH = 0.8
 
-# Match depends on ambient turbulence intensity. 7.5% appears close.
+# Match depends on ambient turbulence intensity. 7.5% appears close. Also, switch off meandering.
 TI = 0.075
+fmodel.set_param(["wake", "wake_velocity_parameters", "eddy_viscosity", "wd_std_ev"], 0.0)
 
 turb_1 = build_cosine_loss_turbine_dict(
     turbine_data_dict={
