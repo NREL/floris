bb38c5ce5b54e0c6d867ef8332754c1703444e3e
misi9170 <michael.sinner@nrel.gov>
2024-08-01 11:04:48 -0600

    Consolidate and rename examples.

--- examples/examples_heterogeneous/00x_wind_direction_heterogeneity_visualization.py
+++ examples/examples_heterogeneous/00x_wind_direction_heterogeneity_visualization.py
@@ -13,14 +13,17 @@ from floris import FlorisModel
 
 fmodel = FlorisModel("../inputs/gch.yaml")
 
+viz_by_sweep = False # TODO: sweep not working correctly.
+# Maybe be to do with rotor points, or how calculate_horizontal_plane_with_turbines is implemented
+
 # # Some wake models may not yet have a visualization method included, for these cases can use
 # # a slower version which scans a turbine model to produce the horizontal flow
-x = np.linspace(-100, 1000, 8)
-v = np.linspace(0, 7, 8).reshape(1, 8)
+x = np.array([-100, 0, 100, 200, 300, 400, 500, 600])
+v = np.array([[0, 1, 2, 3, 4, 5, 6, 7]])
 
 het_inflow_config = {
     "x": np.repeat(x, 2),
-    "y": np.tile(np.array([-200.0, 200.0]), 8),
+    "y": np.tile(np.array([-100.0, 100.0]), 8),
     #"z": np.array([90.0, 90.0, 90.0, 91.0]),
     "speed_multipliers": 1.0*np.ones((1, 16)),
     "u": 8.0*np.ones((1, v.shape[1]*2)),
@@ -30,28 +33,39 @@ het_inflow_config = {
 
 # Set a 2 turbine layout
 fmodel.set(
-    layout_x=[0, 500, 0, 500],
-    layout_y=[0, 0, -100, -100],
+    layout_x=[0, 300],
+    layout_y=[0, 0],
     wind_directions=[270],
     wind_speeds=[8],
     turbulence_intensities=[0.06],
     heterogeneous_inflow_config=het_inflow_config,
 )
 
-horizontal_plane_scan_turbine = flowviz.calculate_horizontal_plane_with_turbines(
-    fmodel,
-    x_resolution=20,
-    y_resolution=10,
-    x_bounds=(-100, 1000),
-    y_bounds=(-200, 200),
-)
+if viz_by_sweep:
+    horizontal_plane = flowviz.calculate_horizontal_plane_with_turbines(
+        fmodel,
+        x_resolution=20,
+        y_resolution=10,
+        x_bounds=(-100, 500),
+        y_bounds=(-100, 100),
+    )
+    title = "Coarse turbine scan method"
+else:
+    horizontal_plane = fmodel.calculate_horizontal_plane(
+        x_resolution=200,
+        y_resolution=100,
+        height=90.0,
+        x_bounds=(-100, 500),
+        y_bounds=(-100, 100),
+    )
+    title = "Standard flow visualization calculation"
 
 fig, ax = plt.subplots(figsize=(10, 4))
 flowviz.visualize_cut_plane(
-    horizontal_plane_scan_turbine,
+    horizontal_plane,
     ax=ax,
     label_contours=True,
-    title="Horizontal (coarse turbine scan method)",
+    title=title
 )
 
 
