348f9cb63ca244921a1195e0f0f851e52bb2c0e5
aclerc <Alex.Clerc@res-group.com>
2024-08-05 16:55:54 +0100

    minor fixes to examples_uncertain

--- examples/examples_uncertain/002_approx_floris_model.py
+++ examples/examples_uncertain/002_approx_floris_model.py
@@ -8,13 +8,11 @@ from time import perf_counter as timerpc
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from floris import (
     ApproxFlorisModel,
     FlorisModel,
     TimeSeries,
 )
-
 
 # Generate time series data using a random walk on wind speeds with constant wind direction
 N = 5000
@@ -22,17 +20,17 @@ n_turbines = 25
 
 # Random walk on wind speed with values between 5 and 20 m/s
 ws = np.ones(N) * 10
+rng = np.random.default_rng(0)
+ws_ll = 5
+ws_ul = 20
 for i in range(1, N):
-    ws[i] = ws[i - 1] + np.random.normal(0, 0.25)
-    if ws[i] < 5:
-        ws[i] = 5
-    if ws[i] > 20:
-        ws[i] = 20
+    ws[i] = ws[i - 1] + rng.normal(0, 0.25)
+    if ws[i] < ws_ll:
+        ws[i] = ws_ll
+    if ws[i] > ws_ul:
+        ws[i] = ws_ul
 
-time_series = TimeSeries(
-    wind_directions=270.,
-    wind_speeds=ws,
-    turbulence_intensities=0.06)
+time_series = TimeSeries(wind_directions=270.0, wind_speeds=ws, turbulence_intensities=0.06)
 
 # Instantiate a FlorisModel and an ApproxFlorisModel
 fmodel = FlorisModel("../inputs/gch.yaml")
@@ -40,7 +38,7 @@ afmodel = ApproxFlorisModel("../inputs/gch.yaml", ws_resolution=0.5)
 
 
 # Set both models to an n_turbine layout and use the above time series
-layout_x = np.array([i*500 for i in range(n_turbines)])
+layout_x = np.array([i * 500 for i in range(n_turbines)])
 layout_y = np.zeros(n_turbines)
 fmodel.set(layout_x=layout_x, layout_y=layout_y, wind_data=time_series)
 afmodel.set(layout_x=layout_x, layout_y=layout_y, wind_data=time_series)
@@ -63,7 +61,7 @@ ax.plot(afmodel.get_farm_power(), label="ApproxFlorisModel")
 ax.set_xlabel("Time Step")
 ax.set_ylabel("Farm Power [W]")
 ax.legend()
-ax.grid(True)
+ax.grid()
 
 
 # Compare the expected power results
@@ -71,3 +69,4 @@ print(f"Expected power from FlorisModel: {fmodel.get_expected_farm_power()/1E6:0
 print(f"Expected power from ApproxFlorisModel: {afmodel.get_expected_farm_power()/1E6:0.2f} MW")
 
 plt.show()
+plt.close()
