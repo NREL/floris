20677e0df1e99caaec5129c26b3e81ebb1628c61
misi9170 <michael.sinner@nrel.gov>
2024-10-07 14:21:23 -0600

    Some typo fixes

--- examples/009_parallel_models.py
+++ examples/009_parallel_models.py
@@ -1,9 +1,9 @@
 """Example 9: Parallel Models
 
 This example demonstrates how to use the ParFlorisModel class to parallelize the
-calculation of the FLORIS model.  ParFlorisModel inherits from the FlorisModel
-and so can be used in the same way with a consistent interface.The ParFlorisModel
- replaces the ParallelFlorisModel
+calculation of the FLORIS model. ParFlorisModel inherits from the FlorisModel
+and so can be used in the same way with a consistent interface. ParFlorisModel
+replaces the ParallelFlorisModel, which will be deprecated in a future release.
 
 """
 
@@ -23,21 +23,21 @@ if __name__ == "__main__":
     # Instantiate the FlorisModel
     fmodel = FlorisModel("inputs/gch.yaml")
 
-    # The ParFlorisModel can be instatiated either from a FlorisModel or from
+    # The ParFlorisModel can be instantiated either from a FlorisModel or from
     # the input file.
     pfmodel_1 = ParFlorisModel("inputs/gch.yaml")  # Via input file
     pfmodel_2 = ParFlorisModel(fmodel)  # Via FlorisModel
 
     # The ParFlorisModel has additional inputs which define the parallelization
-    # but don't effect the output.
+    # but don't affect the output.
     pfmodel_3 = ParFlorisModel(
         fmodel,
         interface="multiprocessing",  # Default
         max_workers=2,  # Defaults to num_cpu
-        n_wind_condition_splits=2,  # Defaults to max_workers)
+        n_wind_condition_splits=2,  # Defaults to max_workers
     )
 
-    # Define a simple inflow with just 1 wind speed
+    # Define a simple inflow
     time_series = TimeSeries(
         wind_speeds=np.arange(1, 25, 0.5), wind_directions=270.0, turbulence_intensities=0.06
     )
@@ -72,8 +72,8 @@ if __name__ == "__main__":
         f"close: {np.allclose(powers_fmodel, powers_pfmodel_3)}"
     )
 
-    # Given that ParFlorisModel is a subclass of FlorisModel, it can also be used as
-    # an input to the UncertainFlorisModel class.  This allows for parallelization of
+    # Because ParFlorisModel is a subclass of FlorisModel, it can also be used as
+    # an input to the UncertainFlorisModel class. This allows for parallelization of
     # the uncertainty calculations.
     ufmodel = UncertainFlorisModel(fmodel)
     pufmodel = UncertainFlorisModel(pfmodel_1)
