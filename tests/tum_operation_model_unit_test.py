adf35cb4866465d30f4f3d0a86ba2fa1a14a23fc
misi9170 <michael.sinner@nrel.gov>
2024-11-05 12:27:25 -0700

    Add reg test for development purposes.

--- tests/tum_operation_model_unit_test.py
+++ tests/tum_operation_model_unit_test.py
@@ -127,3 +127,128 @@ def test_TUMLossTurbine():
     )
     tilt_angles_test - turbine_data["power_thrust_table"]["ref_tilt"]
     #assert test_Ct == baseline_Ct * cosd(yaw_angles_test) * cosd(absolute_tilt)
+
+def test_TUMLossTurbine_regression():
+    """
+    Adding a regression test so that we can work with the model and stay confident that results
+    are not changing.
+    """
+
+    n_turbines = 1
+    wind_speed = 10.0
+    turbine_data = SampleInputs().turbine
+    turbine_data["power_thrust_table"] = SampleInputs().tum_loss_turbine_power_thrust_table
+
+    N_test = 20
+    tilt_angles_nom = turbine_data["power_thrust_table"]["ref_tilt"] * np.ones((N_test, n_turbines))
+    power_setpoints_nom = POWER_SETPOINT_DEFAULT * np.ones((N_test, n_turbines))
+
+    yaw_max = 30 # Maximum yaw to test
+    yaw_angles_test = np.linspace(-yaw_max, yaw_max, N_test).reshape(-1,1)
+
+    power_base = np.array([
+        2393927.66234718,
+        2540214.34341772,
+        2669081.37915352,
+        2781432.03897903,
+        2878018.91106399,
+        2958794.44370780,
+        3023377.59580264,
+        3071678.43043693,
+        3103774.85736505,
+        3119784.56415652,
+        3119784.56415652,
+        3103774.85736505,
+        3071678.43043693,
+        3023377.59580264,
+        2958794.44370780,
+        2878018.91106399,
+        2781432.03897903,
+        2669081.37915352,
+        2540214.34341772,
+        2393927.66234718,
+    ])
+
+    thrust_coefficient_base = np.array([
+        0.65242964,
+        0.68226378,
+        0.70804882,
+        0.73026438,
+        0.74577423,
+        0.75596131,
+        0.76411955,
+        0.77024367,
+        0.77432917,
+        0.77637278,
+        0.77637278,
+        0.77432917,
+        0.77024367,
+        0.76411955,
+        0.75596131,
+        0.74577423,
+        0.73026438,
+        0.70804882,
+        0.68226378,
+        0.65242964,
+    ])
+
+    axial_induction_base = np.array([
+        0.20555629,
+        0.21851069,
+        0.23020638,
+        0.24070476,
+        0.24829308,
+        0.25340393,
+        0.25757444,
+        0.26075276,
+        0.26289671,
+        0.26397642,
+        0.26397642,
+        0.26289671,
+        0.26075276,
+        0.25757444,
+        0.25340393,
+        0.24829308,
+        0.24070476,
+        0.23020638,
+        0.21851069,
+        0.20555629,
+    ])
+
+    power = TUMLossTurbine.power(
+        power_thrust_table=turbine_data["power_thrust_table"],
+        velocities=wind_speed * np.ones((N_test, n_turbines, 3, 3)), # 1 findex, 1 turbine, 3x3 grid
+        air_density=1.1,
+        yaw_angles=yaw_angles_test,
+        power_setpoints=power_setpoints_nom,
+        tilt_angles=tilt_angles_nom,
+        tilt_interp=None
+    ).squeeze()
+
+    thrust_coefficient = TUMLossTurbine.thrust_coefficient(
+        power_thrust_table=turbine_data["power_thrust_table"],
+        velocities=wind_speed * np.ones((N_test, n_turbines, 3, 3)), # 1 findex, 1 turbine, 3x3 grid
+        air_density=1.1,
+        yaw_angles=yaw_angles_test,
+        power_setpoints=power_setpoints_nom,
+        tilt_angles=tilt_angles_nom,
+        tilt_interp=None
+    ).squeeze()
+
+    axial_induction = TUMLossTurbine.axial_induction(
+        power_thrust_table=turbine_data["power_thrust_table"],
+        velocities=wind_speed * np.ones((N_test, n_turbines, 3, 3)), # 1 findex, 1 turbine, 3x3 grid
+        air_density=1.1,
+        yaw_angles=yaw_angles_test,
+        power_setpoints=power_setpoints_nom,
+        tilt_angles=tilt_angles_nom,
+        tilt_interp=None
+    ).squeeze()
+
+    # print(power)
+    # print(thrust_coefficient)
+    # print(axial_induction)
+
+    assert np.allclose(power, power_base)
+    assert np.allclose(thrust_coefficient, thrust_coefficient_base)
+    assert np.allclose(axial_induction, axial_induction_base)
