b62a04d3f783583bae206cba5f864f058649bc95
misi9170 <michael.sinner@nrel.gov>
2024-11-25 09:02:53 -0700

    Add 5mw and 10mw cp ct data (tests failing).

--- tests/conftest.py
+++ tests/conftest.py
@@ -401,7 +401,7 @@ class SampleInputs:
             "beta": -0.45891,
             "cd": 0.0040638,
             "cl_alfa": 4.275049,
-            "cp_ct_data_file": "iea_15MW_cp_ct_surface.npz"
+            "cp_ct_data_file": "nrel_5MW_cp_ct_surface.npz"
         }
 
         self.turbine_floating = copy.deepcopy(self.turbine)
