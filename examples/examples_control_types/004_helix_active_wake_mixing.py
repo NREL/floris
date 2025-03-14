e8fb0b4d01755edf2351b5c07d98c658a62b4440
Gopal Yalla <gyalla@sandia.gov>
2025-03-14 13:06:33 -0600

    Fixing shape of awc_modes in control example 4

--- examples/examples_control_types/004_helix_active_wake_mixing.py
+++ examples/examples_control_types/004_helix_active_wake_mixing.py
@@ -97,6 +97,7 @@ awc_amplitudes = np.array([
     np.linspace(0, 5, N),
     np.zeros(N), np.zeros(N), np.zeros(N)
     ]).reshape(4, N).T
+awc_modes = np.array([["helix", "baseline", "baseline", "baseline"]]*N)
 
 # Reset FlorisModel for different helix amplitudes
 fmodel.set(
