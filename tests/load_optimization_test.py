3b458e16d1b82c23117e98f5b3306ff01a67a211
Paul <paul.fleming@nrel.gov>
2025-03-31 12:38:27 -0600

    Update test names

--- tests/load_optimization_test.py
+++ tests/load_optimization_test.py
@@ -8,7 +8,7 @@ from floris import (
 from floris.optimization.load_optimization.load_optimization import (
     compute_farm_revenue,
     compute_farm_voc,
-    compute_load_ti,
+    compute_lti,
     compute_net_revenue,
     compute_turbine_voc,
     find_A_to_satisfy_rev_voc_ratio,
@@ -33,7 +33,7 @@ def test_get_rotor_diameters():
     np.testing.assert_allclose(rotor_diameters, [125.88, 125.88], atol=1e-4)
 
 
-def test_compute_load_ti_no_wake():
+def test_compute_lti_no_wake():
     # If we pass in a two-turbine, two-findex case where the turbines are
     # not aligned in flow, would expect to get back np.ones((2,2)) * ambient
     fmodel = FlorisModel(configuration="defaults")
@@ -42,25 +42,25 @@ def test_compute_load_ti_no_wake():
         wind_speeds=[8.0, 8.0], wind_directions=[0.0, 0.0], turbulence_intensities=[0.06, 0.06]
     )
     fmodel.run()
-    load_ambient_tis = [0.12, 0.1]
-    load_ti = compute_load_ti(fmodel, load_ambient_tis)
+    ambient_lti = [0.12, 0.1]
+    load_ti = compute_lti(fmodel, ambient_lti)
     assert load_ti.shape == (2, 2)
-    assert load_ti[0, 0] == load_ambient_tis[0]
-    assert load_ti[0, 1] == load_ambient_tis[0]
-    assert load_ti[1, 0] == load_ambient_tis[1]
-    assert load_ti[1, 1] == load_ambient_tis[1]
+    assert load_ti[0, 0] == ambient_lti[0]
+    assert load_ti[0, 1] == ambient_lti[0]
+    assert load_ti[1, 0] == ambient_lti[1]
+    assert load_ti[1, 1] == ambient_lti[1]
 
 
-def test_compute_load_ti_wake():
+def test_compute_lti_wake():
     # Test two turbines in a wake, n_findex = 1
     fmodel = FlorisModel(configuration="defaults")
     fmodel.set(layout_x=[0, 500.0], layout_y=[0.0, 0.0])
     fmodel.set(wind_speeds=[8.0], wind_directions=[270.0], turbulence_intensities=[0.06])
     fmodel.run()
-    load_ambient_tis = [0.1]
-    load_ti = compute_load_ti(fmodel, load_ambient_tis)
+    ambient_lti = [0.1]
+    load_ti = compute_lti(fmodel, ambient_lti)
     assert load_ti.shape == (1, 2)
-    np.testing.assert_almost_equal(load_ti[0, 0], load_ambient_tis)
+    np.testing.assert_almost_equal(load_ti[0, 0], ambient_lti)
     assert load_ti[0, 1] > load_ti[0, 0]
 
 
@@ -74,8 +74,8 @@ def test_compute_turbine_voc_no_wake():
         wind_speeds=[8.0, 8.0], wind_directions=[0.0, 0.0], turbulence_intensities=[0.06, 0.06]
     )
     fmodel.run()
-    load_ambient_tis = [0.12, 0.12]
-    voc = compute_turbine_voc(fmodel, 0.01, load_ambient_tis)
+    ambient_lti = [0.12, 0.12]
+    voc = compute_turbine_voc(fmodel, 0.01, ambient_lti)
     assert voc.shape == (2, 2)
     assert voc[0, 0] == voc[0, 1]
     assert voc[0, 0] == voc[1, 0]
@@ -88,8 +88,8 @@ def test_compute_turbine_voc_wake():
     fmodel.set(layout_x=[0, 500.0], layout_y=[0.0, 0.0])
     fmodel.set(wind_speeds=[8.0], wind_directions=[270.0], turbulence_intensities=[0.06])
     fmodel.run()
-    load_ambient_tis = [0.1]
-    voc = compute_turbine_voc(fmodel, 0.01, load_ambient_tis)
+    ambient_lti = [0.1]
+    voc = compute_turbine_voc(fmodel, 0.01, ambient_lti)
     assert voc.shape == (1, 2)
     assert voc[0, 1] > voc[0, 0]
 
@@ -108,8 +108,8 @@ def test_compute_net_revenue_no_wake():
 
     fmodel.set(wind_data=time_series)
     fmodel.run()
-    load_ambient_tis = [0.12, 0.12]
-    net_revenue = compute_net_revenue(fmodel, 1, load_ambient_tis)
+    ambient_lti = [0.12, 0.12]
+    net_revenue = compute_net_revenue(fmodel, 1, ambient_lti)
     assert net_revenue.shape == (2,)
     assert net_revenue[0] == net_revenue[1]
 
@@ -129,13 +129,13 @@ def test_find_A_to_satisfy_rev_voc_ratio():
     fmodel.set(wind_data=time_series)
     fmodel.run()
 
-    load_ambient_tis = np.ones(N) * 0.1
+    ambient_lti = np.ones(N) * 0.1
 
-    A = find_A_to_satisfy_rev_voc_ratio(fmodel, target_rev_voc_ratio, load_ambient_tis)
+    A = find_A_to_satisfy_rev_voc_ratio(fmodel, target_rev_voc_ratio, ambient_lti)
 
     farm_revenue = compute_farm_revenue(fmodel)
 
-    farm_voc = compute_farm_voc(fmodel, A, load_ambient_tis)
+    farm_voc = compute_farm_voc(fmodel, A, ambient_lti)
 
     assert np.allclose(farm_revenue.sum() / farm_voc.sum(), target_rev_voc_ratio, atol=1e-4)
 
@@ -155,12 +155,12 @@ def test_find_A_to_satisfy_target_VOC_per_MW():
     fmodel.set(wind_data=time_series)
     fmodel.run()
 
-    load_ambient_tis = np.ones(N) * 0.1
+    ambient_lti = np.ones(N) * 0.1
 
-    A = find_A_to_satisfy_target_VOC_per_MW(fmodel, target_VOC_per_MW_findex, load_ambient_tis)
+    A = find_A_to_satisfy_target_VOC_per_MW(fmodel, target_VOC_per_MW_findex, ambient_lti)
 
     farm_power = fmodel.get_farm_power()
 
-    farm_voc = compute_farm_voc(fmodel, A, load_ambient_tis)
+    farm_voc = compute_farm_voc(fmodel, A, ambient_lti)
 
     assert np.allclose(1e6 * farm_voc.sum() / farm_power.sum(), target_VOC_per_MW_findex, atol=1e-4)
