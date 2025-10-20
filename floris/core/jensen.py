f7fdeacf5bf106042c96a73eefcca251c964de61
misi9170 <michael.sinner@nrel.gov>
2025-10-20 07:49:00 -0600

    Move to an abstract base model

--- floris/core/jensen.py
+++ floris/core/jensen.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 import numexpr as ne
 import numpy as np
 
@@ -22,7 +24,91 @@ NUM_EPS = fields(BaseModel).NUM_EPS.default
 # TODO: I'll likely want an abstract base class; can work on that later
 
 @define
-class JensenJimenez():
+class BaseWakeModel():
+    def set_turbine_i(self, grid, farm, i):
+
+        # Get the current turbine quantities
+        self.x_i = np.mean(grid.x_sorted[:, i:i+1], axis=(2, 3), keepdims=True)
+        self.y_i = np.mean(grid.y_sorted[:, i:i+1], axis=(2, 3), keepdims=True)
+        self.z_i = np.mean(grid.z_sorted[:, i:i+1], axis=(2, 3), keepdims=True)
+
+        self.yaw_angle_i = farm.yaw_angles_sorted[:, i:i+1, None, None]
+        self.hub_height_i = farm.hub_heights_sorted[:, i:i+1, None, None]
+        self.rotor_diameter_i = farm.rotor_diameters_sorted[:, i:i+1, None, None]
+
+    @staticmethod
+    def turbine_thrust_coefficient(grid, farm, flow_field, i):
+        ct_i = thrust_coefficient(
+            velocities=flow_field.u_sorted,
+            turbulence_intensities=flow_field.turbulence_intensity_field_sorted,
+            air_density=flow_field.air_density,
+            yaw_angles=farm.yaw_angles_sorted,
+            tilt_angles=farm.tilt_angles_sorted,
+            power_setpoints=farm.power_setpoints_sorted,
+            awc_modes=farm.awc_modes_sorted,
+            awc_amplitudes=farm.awc_amplitudes_sorted,
+            thrust_coefficient_functions=farm.turbine_thrust_coefficient_functions,
+            tilt_interps=farm.turbine_tilt_interps,
+            correct_cp_ct_for_tilt=farm.correct_cp_ct_for_tilt_sorted,
+            turbine_type_map=farm.turbine_type_map_sorted,
+            turbine_power_thrust_tables=farm.turbine_power_thrust_tables,
+            ix_filter=[i],
+            average_method=grid.average_method,
+            cubature_weights=grid.cubature_weights,
+            multidim_condition=flow_field.multidim_conditions
+        )
+        # Since we are filtering for the i'th turbine in the thrust coefficient function,
+        # get the first index here (0:1)
+        return ct_i[:, 0:1, None, None]
+
+    @abstractmethod
+    def turbine_solve(
+        self,
+        farm: Farm,
+        flow_field: FlowField,
+        grid: TurbineGrid,
+    ) -> None:
+        raise NotImplementedError(
+            "The turbine_solve method has not yet been implemented for "+self.__class__.__name__
+        )
+
+    @abstractmethod
+    def point_solve(
+        self,
+        farm: Farm,
+        flow_field: FlowField,
+        flow_field_grid: FlowFieldGrid | FlowFieldPlanarGrid | PointsGrid,
+    ):
+        raise NotImplementedError(
+            "points_solve is not implemented for "+self.__class__.__name__
+        )
+
+    @staticmethod
+    def turbine_axial_induction(grid, farm, flow_field, i):
+        axial_induction_i = axial_induction(
+            velocities=flow_field.u_sorted,
+            turbulence_intensities=flow_field.turbulence_intensity_field_sorted,
+            air_density=flow_field.air_density,
+            yaw_angles=farm.yaw_angles_sorted,
+            tilt_angles=farm.tilt_angles_sorted,
+            power_setpoints=farm.power_setpoints_sorted,
+            awc_modes=farm.awc_modes_sorted,
+            awc_amplitudes=farm.awc_amplitudes_sorted,
+            axial_induction_functions=farm.turbine_axial_induction_functions,
+            tilt_interps=farm.turbine_tilt_interps,
+            correct_cp_ct_for_tilt=farm.correct_cp_ct_for_tilt_sorted,
+            turbine_type_map=farm.turbine_type_map_sorted,
+            turbine_power_thrust_tables=farm.turbine_power_thrust_tables,
+            ix_filter=[i],
+            average_method=grid.average_method,
+            cubature_weights=grid.cubature_weights,
+            multidim_condition=flow_field.multidim_conditions
+        )
+
+        return axial_induction_i[:, 0:1, None, None]
+
+@define
+class JensenJimenez(BaseWakeModel):
 
     # Jensen deficit model parameters
     we: float = field(default=0.05)
@@ -293,66 +379,3 @@ class JensenJimenez():
             flow_field.u_sorted = flow_field.u_initial_sorted - wake_field
             flow_field.v_sorted += v_wake
             flow_field.w_sorted += w_wake
-
-    def set_turbine_i(self, grid, farm, i):
-        # TODO: Candidate for method on base model
-
-        # Get the current turbine quantities
-        self.x_i = np.mean(grid.x_sorted[:, i:i+1], axis=(2, 3), keepdims=True)
-        self.y_i = np.mean(grid.y_sorted[:, i:i+1], axis=(2, 3), keepdims=True)
-        self.z_i = np.mean(grid.z_sorted[:, i:i+1], axis=(2, 3), keepdims=True)
-
-        self.yaw_angle_i = farm.yaw_angles_sorted[:, i:i+1, None, None]
-        self.hub_height_i = farm.hub_heights_sorted[:, i:i+1, None, None]
-        self.rotor_diameter_i = farm.rotor_diameters_sorted[:, i:i+1, None, None]
-
-    @staticmethod
-    def turbine_thrust_coefficient(grid, farm, flow_field, i):
-        # TODO: put on base model
-        ct_i = thrust_coefficient(
-            velocities=flow_field.u_sorted,
-            turbulence_intensities=flow_field.turbulence_intensity_field_sorted,
-            air_density=flow_field.air_density,
-            yaw_angles=farm.yaw_angles_sorted,
-            tilt_angles=farm.tilt_angles_sorted,
-            power_setpoints=farm.power_setpoints_sorted,
-            awc_modes=farm.awc_modes_sorted,
-            awc_amplitudes=farm.awc_amplitudes_sorted,
-            thrust_coefficient_functions=farm.turbine_thrust_coefficient_functions,
-            tilt_interps=farm.turbine_tilt_interps,
-            correct_cp_ct_for_tilt=farm.correct_cp_ct_for_tilt_sorted,
-            turbine_type_map=farm.turbine_type_map_sorted,
-            turbine_power_thrust_tables=farm.turbine_power_thrust_tables,
-            ix_filter=[i],
-            average_method=grid.average_method,
-            cubature_weights=grid.cubature_weights,
-            multidim_condition=flow_field.multidim_conditions
-        )
-        # Since we are filtering for the i'th turbine in the thrust coefficient function,
-        # get the first index here (0:1)
-        return ct_i[:, 0:1, None, None]
-
-    @staticmethod
-    def turbine_axial_induction(grid, farm, flow_field, i):
-        # TODO: put on base model
-        axial_induction_i = axial_induction(
-            velocities=flow_field.u_sorted,
-            turbulence_intensities=flow_field.turbulence_intensity_field_sorted,
-            air_density=flow_field.air_density,
-            yaw_angles=farm.yaw_angles_sorted,
-            tilt_angles=farm.tilt_angles_sorted,
-            power_setpoints=farm.power_setpoints_sorted,
-            awc_modes=farm.awc_modes_sorted,
-            awc_amplitudes=farm.awc_amplitudes_sorted,
-            axial_induction_functions=farm.turbine_axial_induction_functions,
-            tilt_interps=farm.turbine_tilt_interps,
-            correct_cp_ct_for_tilt=farm.correct_cp_ct_for_tilt_sorted,
-            turbine_type_map=farm.turbine_type_map_sorted,
-            turbine_power_thrust_tables=farm.turbine_power_thrust_tables,
-            ix_filter=[i],
-            average_method=grid.average_method,
-            cubature_weights=grid.cubature_weights,
-            multidim_condition=flow_field.multidim_conditions
-        )
-
-        return axial_induction_i[:, 0:1, None, None]
