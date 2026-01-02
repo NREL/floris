df191071fd3ae43de3aa0e0f29a6085ae1439c78
bayc <christopher.j.bay@gmail.com>
2026-01-02 14:36:37 -0700

    correction in SAWS calculation

--- floris/core/flow_field.py
+++ floris/core/flow_field.py
@@ -346,10 +346,10 @@ class FlowField(BaseClass):
             vels = self.u[:, i]
 
             # simple average
-            self.SAWS[:, i, 0] = np.mean(vels[0, 0, :])
-            self.SAWS[:, i, 1] = np.mean(vels[0, :, -1])
-            self.SAWS[:, i, 2] = np.mean(vels[0, -1, :])
-            self.SAWS[:, i, 3] = np.mean(vels[0, :, 0])
+            self.SAWS[:, i, 0] = np.mean(vels[:, 0, :])
+            self.SAWS[:, i, 1] = np.mean(vels[:, :, -1])
+            self.SAWS[:, i, 2] = np.mean(vels[:, -1, :])
+            self.SAWS[:, i, 3] = np.mean(vels[:, :, 0])
 
             # weighted average
             # self.core.flow_field.SAWS[:, i, 0] = (
